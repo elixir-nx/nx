f356b92620e2fa4c642658e591c92714a1a63670
Sean Moriarity <smoriarity.5@gmail.com>
2020-10-29 21:01:33 -0400

    basic building functions

--- lib/exla/builder.ex
+++ lib/exla/builder.ex
@@ -1,8 +1,6 @@
 defmodule Exla.Builder do
   @moduledoc """
   Wrapper around the xla::XlaBuilder class.
-
-  The Builder object is created on application startup and maintained inside a GenServer.
   """
 
   @on_load :load_nifs
@@ -10,49 +8,11 @@ defmodule Exla.Builder do
   app = Mix.Project.config()[:app]
   @doc false
   def load_nifs do
-    path = :filename.join(:code.priv_dir(unquote(app)), 'bazel-bin/exla/libexla')
+    path = :filename.join(:code.priv_dir(unquote(app)), 'bazel-bin/exla/libbuilder')
     :erlang.load_nif(path, 0)
   end
 
-  alias __MODULE__, as: Builder
-
-  defstruct [:name, :ref]
-
-  use GenServer
-
-  def start_link(name) do
-    GenServer.start_link(__MODULE__, name, name: __MODULE__)
-  end
-
-  def build do
-    GenServer.call(__MODULE__, :build)
-  end
-
-  def get do
-    GenServer.call(__MODULE__, :get)
-  end
-
-  @impl true
-  def init(name) do
-    builder = Builder.create_builder(name)
-    {:ok, struct(Builder, builder)}
-  end
-
-  @impl true
-  def handle_call(:get, _from, %Builder{} = builder) do
-    {:reply, builder.ref, builder}
-  end
-
-  @impl true
-  def handle_call(:build, _from, %Builder{} = builder) do
-    {:reply, _build(builder.ref), builder}
-  end
-
-  @doc false
-  def _build(_ref),
-    do: raise("Failed to load implementation #{__MODULE__}._build/0")
-
-  @doc false
-  def create_builder(_name),
-    do: raise("Failed to load implementation #{__MODULE__}.create_builder/1")
+  def add(_a, _b), do: raise "BAD"
+  def build, do: raise "BAD"
+  def constant_from_array, do: raise "BAD"
 end
--- src/exla/builder.cc
+++ src/exla/builder.cc
@@ -1,28 +1,98 @@
 #include "absl/memory/memory.h"
+#include "absl/types/span.h"
+#include "tensorflow/compiler/xla/literal_util.h"
 #include "tensorflow/compiler/xla/statusor.h"
 #include "tensorflow/compiler/xla/client/xla_builder.h"
 #include "tensorflow/compiler/xla/client/xla_computation.h"
 #include <erl_nif.h>
 
-ERL_NIF_TERM create_builder(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
-  auto builder = absl::make_unique<xla::XlaBuilder>("elixir");
-  return enif_make_binary(env, (ErlNifBinary*) &builder);
+ErlNifResourceType* BUILDER_RES_TYPE;
+ErlNifResourceType* OP_RES_TYPE;
+ERL_NIF_TERM ok;
+
+typedef struct {
+  xla::XlaBuilder* builder;
+} Builder;
+
+typedef struct {
+  xla::XlaOp op;
+} Op;
+
+void free_builder(ErlNifEnv* env, void* obj){
+  // enif_release_resource(obj);
 }
 
-ERL_NIF_TERM build(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
-  ErlNifBinary* builder_ptr;
-  enif_inspect_binary(env, argv[0], builder_ptr);
+void free_op(ErlNifEnv* env, void* obj){
+  enif_release_resource(obj);
+}
 
-  xla::XlaBuilder& builder = (xla::XlaBuilder&) *builder_ptr;
-  builder.Build();
+static int load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info){
+  const char* mod = "builder";
+  const char* name_builder = "Builder";
+  const char* name_op = "Op";
 
-  return enif_make_atom(env, "ok");
+  int flags = ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER;
+
+  Builder* builder;
+
+  BUILDER_RES_TYPE = enif_open_resource_type(env, mod, name_builder, free_builder, (ErlNifResourceFlags) flags, NULL);
+  OP_RES_TYPE = enif_open_resource_type(env, mod, name_op, free_op, (ErlNifResourceFlags) flags, NULL);
+  if(BUILDER_RES_TYPE == NULL || OP_RES_TYPE == NULL) return -1;
+
+  builder = (Builder*) enif_alloc(sizeof(Builder));
+  ok = enif_make_atom(env, "ok");
+
+  builder->builder = new xla::XlaBuilder("Elixir");
+
+  *priv = (void*) builder;
+
+  return 0;
+}
+
+ERL_NIF_TERM enif_make_op(ErlNifEnv* env, xla::XlaOp value){
+  Op* op = (Op*) enif_alloc_resource(OP_RES_TYPE, sizeof(Op*));
+  op->op = value;
+  ERL_NIF_TERM ret = enif_make_resource(env, op);
+  enif_keep_resource(op);
+
+  return ret;
+}
+
+ERL_NIF_TERM constant_from_array(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  Builder* builder = (Builder*) enif_priv_data(env);
+
+  xla::Array<uint> arr = xla::Array<uint>({1, 2, 3});
+  xla::XlaOp op = xla::ConstantFromArray(builder->builder, arr);
+
+  return enif_make_op(env, op);
+}
+
+ERL_NIF_TERM add(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  Op *lhs, *rhs;
+  enif_get_resource(env, argv[0], OP_RES_TYPE, (void **) &lhs);
+  enif_get_resource(env, argv[1], OP_RES_TYPE, (void **) &rhs);
+
+  absl::Span<const long long int> broadcast = absl::Span<const long long int>({1});
+
+  xla::XlaOp result = xla::Add(lhs->op, rhs->op, broadcast);
+  return enif_make_op(env, result);
+}
+
+ERL_NIF_TERM build(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  Builder* builder = (Builder*) enif_priv_data(env);
+
+  builder->builder->Build();
+
+  return ok;
 }
 
 static ErlNifFunc builder_funcs[] = {
-  {"create_builder", 1, create_builder},
-  {"_build", 1, build}
+  {"add", 2, add},
+  {"constant_from_array", 0, constant_from_array},
+  {"build", 0, build}
 };
 
-
-ERL_NIF_INIT(Elixir.Exla.Builder, builder_funcs, NULL, NULL, NULL, NULL);
+ERL_NIF_INIT(Elixir.Exla.Builder, builder_funcs, &load, NULL, NULL, NULL);
