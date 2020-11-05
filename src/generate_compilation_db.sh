693c247520ce1e4530084e233adea7fdb46de04d
Tsai <caishenghang@oneflow.org>
2020-11-05 20:48:38 +0800

    fix command jso

--- src/generate_compilation_db.sh
+++ src/generate_compilation_db.sh
@@ -2,8 +2,8 @@
 bazel build exla:exla_compdb
 
 # Location of the compilation database file.
-outfile="$(bazel info bazel-bin)/exla/compile_commands.json"
-
+outfile="${PWD}/compile_commands.json"
+touch $outfile
 # Command to replace the marker for exec_root in the file.
 execroot=$(bazel info execution_root)
 sed -i.bak "s@__EXEC_ROOT__@${execroot}@" "${outfile}"
