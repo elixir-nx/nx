22b32b9d69c2ccdab613252b2b12914e1d8b0f7a
Sean Moriarity <smoriarity.5@gmail.com>
2020-11-10 14:44:46 -0500

    check in op and update shape, client

--- lib/exla/op.ex
+++ lib/exla/op.ex
@@ -5,21 +5,10 @@ defmodule Exla.Op do
   @enforce_keys [:ref]
   defstruct [:ref]
 
-  def parameter(i, shape, name) when is_integer(i) and i >= 0 and is_binary(name) do
-    case shape do
-      # Alternatively, we can construct the shape for them but they need the reference later
-      # and it's weird if we return it to them here
-      %Shape{ref: nil} ->
-        {:error, "Unconstructed shape. Pass your shape to `Exla.Shape.make_shape/1`"}
-
-      %Shape{ref: ref} ->
-        case Exla.NIF.parameter(i, ref, name) do
-          {:ok, ref} -> {:ok, %Op{ref: ref}}
-          {:error, msg} -> {:error, msg}
-        end
-
-      _ ->
-        {:error, "Invalid shape passed to `#{__MODULE__}.parameter/3.`"}
+  def parameter(i, shape = %Shape{}, name) when is_integer(i) and i >= 0 and is_binary(name) do
+    case Exla.NIF.parameter(i, shape.ref, name) do
+      {:ok, ref} -> {:ok, %Op{ref: ref}}
+      {:error, msg} -> {:error, msg}
     end
   end
 
--- lib/exla/shape.ex
+++ lib/exla/shape.ex
@@ -1,22 +1,14 @@
 defmodule Exla.Shape do
   alias __MODULE__, as: Shape
 
-  @enforce_keys [:dims, :dtype]
+  @enforce_keys [:dims, :dtype, :ref]
   defstruct [:dims, :dtype, :ref]
 
-  def make_shape(shape = %Shape{}) do
-    case shape do
-      %Shape{ref: nil} ->
-        case Exla.NIF.make_shape(shape.dtype, shape.dims) do
-          {:ok, ref} -> {:ok, %Shape{ref: ref | shape}}
-          {:error, msg} -> {:error, msg}
-        end
-
-      %Shape{ref: ref} ->
-        {:error, "Attempting to construct an already constructed shape."}
-
-      _ ->
-        {:error, "Invalid shape passed to `#{__MODULE__}.make_shape/2`"}
+  # TODO: convert dtype to integer representation to use in NIF
+  def make_shape(dtype, dims) when is_atom(dtype) and is_tuple(dims) do
+    case Exla.NIF.make_shape(dtype, dims) do
+      {:ok, ref} -> {:ok, %Shape{ref: ref, dims: dims, dtype: dtype}}
+      {:error, msg} -> {:error, msg}
     end
   end
 end
