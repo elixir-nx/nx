c3213cc928d23982e41c30bfa1ee1c03ca24bdb1
José Valim <jose.valim@dashbit.co>
2020-11-25 23:31:38 +0100

    Clean up some NIF functions (#51)
      * Remove literal<->string functions
      * Remove get_program_shape and instead pass it though computation
      * Remove make_scalar_shape (in favor of make_shape)

--- lib/exla/computation.ex
+++ lib/exla/computation.ex
@@ -2,16 +2,6 @@ defmodule Exla.Computation do
   alias __MODULE__, as: Computation
   alias Exla.Shape
 
-  @enforce_keys [:ref]
-  defstruct [:ref]
-
-  def get_program_shape(%Computation{ref: ref}) do
-    {:ok, {input_shapes, {output_dims, output_type_str, output_ref}}} = Exla.NIF.get_program_shape(ref)
-
-    input_shapes =
-      input_shapes
-      |> Enum.map(fn {dims, type, ref} -> %Shape{dims: dims, dtype: Shape.str_to_dtype(type), ref: ref} end)
-
-    {input_shapes, %Shape{dims: output_dims, dtype: Shape.str_to_dtype(output_type_str), ref: output_ref}}
-  end
+  @enforce_keys [:ref, :output_shape]
+  defstruct [:ref, :output_shape]
 end
