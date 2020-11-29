4eab34f63bcf13a9381b154e8a5c0530012fede4
José Valim <jose.valim@dashbit.co>
2020-11-29 09:23:56 +0100

    Remove outdated and used examples

--- examples/get_shape.exs
+++
@@ -1,7 +0,0 @@
-builder = Exla.Builder.new("builder")
-
-shape = Exla.Shape.make_shape(:float64, [5, 5, 5, 5, 5])
-
-param = Exla.Op.parameter(builder, 0, shape, "x")
-
-IO.inspect Exla.Op.get_shape(param)
