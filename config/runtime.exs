a3e4a943541cd1a1f0c5d953edda7104c0ebbd21
Sean Moriarity <smoriarity.5@gmail.com>
2020-11-30 09:08:09 -0500

    Revert config changes

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,5 +2,4 @@ import Config
 
 target = System.get_env("EXLA_TARGET", "host")
 
-config :exla, :clients, cuda: [platform: :cuda],
-                        host: [platform: :host]
+config :exla, :clients, default: [platform: String.to_atom(target)]
