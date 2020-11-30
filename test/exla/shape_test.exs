56b6e91e8658fbaa5ee2f10f358521589358428a
Sean Moriarity <smoriarity.5@gmail.com>
2020-11-30 08:49:03 -0500

    Remove make_tuple_shape test

--- test/exla/shape_test.exs
+++ test/exla/shape_test.exs
@@ -12,16 +12,4 @@ defmodule Exla.ShapeTest do
       assert %Shape{dtype: {:bf, 16}, dims: {}, ref: _} = Shape.make_shape({:bf, 16}, {})
     end
   end
-
-  test "make_tuple_shape/1" do
-    s1 = Shape.make_shape({:s, 32}, {1, 1})
-    s2 = Shape.make_shape({:s, 32}, {2, 2})
-
-    assert %Shape{
-             dtype:
-               {:t,
-                [%Shape{dtype: {:s, 32}, dims: {1, 1}}, %Shape{dtype: {:s, 32}, dims: {2, 2}}]},
-             dims: {2}
-           } = Shape.make_tuple_shape([s1, s2])
-  end
 end
