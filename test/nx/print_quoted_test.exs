80b63c19f66b293f46c9cdae93b1def8da3db01e
José Valim <jose.valim@dashbit.co>
2020-12-08 10:59:43 +0100

    Tests for print_quoted

---
+++ test/nx/print_quoted_test.exs
@@ -0,0 +1,22 @@
+defmodule Nx.PrintQuotedTest do
+  use ExUnit.Case, async: true
+
+  import ExUnit.CaptureIO
+
+  test "prints the quoted expression" do
+    assert capture_io(fn ->
+             defmodule Example do
+               import Nx.Defn
+               defn print_grad(t), do: print_quoted(grad(t, Nx.power(t, 3)))
+             end
+           end) =~ """
+           (
+             a = Nx.power(b, 3)
+             (
+               c = 3
+               Nx.multiply(c, Nx.power(b, Nx.subtract(c, 1)))
+             )
+           )
+           """
+  end
+end
