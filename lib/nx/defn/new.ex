652717896bae5132d09d317127e7a5c957842f45
Sean Moriarity <smoriarity.5@gmail.com>
2020-12-21 12:56:56 -0500

    Add binary ops and aggregate ops

--- lib/nx/defn/new.ex
+++ lib/nx/defn/new.ex
@@ -21,11 +21,72 @@ defmodule Nx.Defn.Translation do
   # compiler used, therefore any defn backend needs to ensure their
   # operation semantics enforce these translations.
 
-  ## Unary Ops
+  ## Element-wise unary ops
 
-  # The shapes of element-wise unary ops remain unchanged regardless
-  # of the input.
-  def exp(shape), do: shape
+  # The shapes of element-wise unary ops remain
+  # unchanged regardless of the input.
+  def unary_op_rule(shape), do: shape
+
+  ## Element-wise binary ops
+
+  # The shapes of element-wise binary ops are
+  # broadcasted if broadcasting is compatible.
+  def binary_op_rule(s1, s2), do: broadcast(s1, s2)
+
+  ## Aggregate ops
+
+  # The shapes of aggregate ops are contracted along
+  # the specified dimension(s).
+  def aggregate_op_rule(shape, axes), do: contract(shape, axes)
+
+  ## Translation primitives
+
+  def broadcast(shape, shape), do: shape
+
+  def broadcast(s1, s2) when is_tuple(s1) and is_tuple(s2),
+    do: List.to_tuple(do_broadcast(Tuple.to_list(s1), Tuple.to_list(s2)))
+
+  defp do_broadcast(s1, s2) when length(s1) > length(s2) do
+    [dim | s1] = s1
+    [dim | do_broadcast(s1, s2)]
+  end
+
+  defp do_broadcast(s1, s2) when length(s2) > length(s1) do
+    [dim | s2] = s2
+    [dim | do_broadcast(s1, s2)]
+  end
+
+  defp do_broadcast([], s2), do: s2
+  defp do_broadcast(s1, []), do: s1
+  defp do_broadcast([1 | s1], [dim2 | s2]) do
+    [dim2 | do_broadcast(s1, s2)]
+  end
+  defp do_broadcast([dim1 | s1], [1 | s2]) do
+    [dim1 | do_broadcast(s1, s2)]
+  end
+  defp do_broadcast([dim | s1], [dim | s2]) do
+    [dim | do_broadcast(s1, s2)]
+  end
+  defp do_broadcast([dim1 | _s1], [dim2 | _s2]) do
+    raise ArgumentError, "could not broadcast shapes because dimensions are" <>
+                         " incompatible, expected dimensions to be equal or" <>
+                         " either dimension to be 1, got: #{dim1} and #{dim2}"
+  end
+
+  def contract(shape, []), do: shape
+
+  def contract(shape, [axis | []]), do: Tuple.delete_at(shape, axis)
+
+  def contract(shape, axes) when is_list(axes) do
+    shape
+    |> Tuple.to_list()
+    |> Enum.with_index()
+    |> Enum.filter(fn {_, i} -> i not in axes end)
+    |> List.to_tuple()
+  end
+
+  def contract(shape, axis) when is_integer(axis), do: Tuple.delete_at(shape, axis)
+
 end
 
 defmodule Nx.Defn.New do
@@ -61,13 +122,62 @@ defmodule Nx.Defn.New do
 
   ## Operations
 
-  def exp(expr) do
+  def make_unary_op_expr(op, expr) do
     %Expr{shape: shape} = expr = to_expr(expr)
-    output_shape = Translation.exp(shape)
-    make_expr(shape, :exp, [expr])
+    output_shape = Translation.unary_op_rule(shape)
+    make_expr(output_shape, op, [expr])
   end
 
+  def make_binary_op_expr(op, expr1, expr2) do
+    %Expr{shape: s1} = expr1 = to_expr(expr1)
+    %Expr{shape: s2} = expr2 = to_expr(expr2)
+    output_shape = Translation.binary_op_rule(s1, s2)
+    make_expr(output_shape, op, [expr1, expr2])
+  end
+
+  def make_aggregate_op_expr(op, expr, opts) do
+    %Expr{shape: shape} = expr = to_expr(expr)
+    axes = opts[:axes] || opts[:axis] || all_dimensions(shape)
+    output_shape = Translation.aggregate_op_rule(shape, axes)
+    make_expr(output_shape, op, [expr, opts])
+  end
+
+  @element_wise_unary_op [:exp, :expm1, :log,
+                          :logp1, :logistic, :cos,
+                          :sin, :tanh, :sqrt, :rsqrt,
+                          :cbrt, :negate, :sign, :abs,
+                          :bitwise_not, :population_count,
+                          :count_leading_zeros, :floor,
+                          :ceil, :round]
+
+  @element_wise_binary_op [:add, :subtract, :multiply, :divide,
+                           :power, :remainder, :arctan2, :max,
+                           :min, :bitwise_and, :bitwise_or,
+                           :bitwise_xor, :left_shift, :right_shift,
+                           :equal, :not_equal, :greater, :less,
+                           :less_equal, :greater_equal]
+
+  @aggregate_op [:sum, :mean, :argmax, :argmin]
+
   ## Traversal
+
+  defp traverse({{:., dot_meta, [Nx, name]}, meta, args}, state)
+       when name in @element_wise_unary_op do
+    {args, state} = traverse(args, state)
+    {to_expr_call(dot_meta, meta, :make_unary_op_expr, [name | args]), state}
+  end
+
+  defp traverse({{:., dot_meta, [Nx, name]}, meta, args}, state)
+       when name in @element_wise_binary_op do
+    {args, state} = traverse(args, state)
+    {to_expr_call(dot_meta, meta, :make_binary_op_expr, [name | args]), state}
+  end
+
+  defp traverse({{:., dot_meta, [Nx, name]}, meta, args}, state)
+       when name in @aggregate_op do
+    {args, state} = traverse(args, state)
+    {to_expr_call(dot_meta, meta, :make_aggregate_op_expr, [name | args]), state}
+  end
 
   defp traverse({{:., dot_meta, [Nx, name]}, meta, args}, state) do
     {args, state} = traverse(args, state)
@@ -96,11 +206,9 @@ defmodule Nx.Defn.New do
   defp to_expr_call(dot_meta, meta, fun, args) do
     {{:., dot_meta, [__MODULE__, fun]}, meta, args}
   end
-end
 
-defmodule Nx.Defn.Default do
-  @moduledoc false
-
-  defdelegate tensor(t), to: Nx
-  defdelegate exp(t), to: Nx
+  ## Helpers
+  defp all_dimensions(shape) do
+    for i <- 0..(tuple_size(shape) - 1), do: i
+  end
 end
--- test/nx/defn_new_test.exs
+++ test/nx/defn_new_test.exs
@@ -3,14 +3,62 @@ defmodule DefnNewTest do
 
   import Nx.Defn
   alias Nx.Defn.Expr
+  alias Nx.Defn.Translation
 
   @default_defn_compiler Nx.Defn.New
 
-  describe "exp" do
+  describe "unary ops" do
     defn exp(t), do: Nx.exp(t)
 
     test "to expr" do
       assert %Expr{op: :exp, args: [_], shape: {3}} = exp(Nx.tensor([1, 2, 3]))
+    end
+  end
+
+  describe "binary ops" do
+    defn add(t1, t2), do: Nx.add(t1, t2)
+
+    test "to expr" do
+      assert %Expr{op: :add, args: [_, _], shape: {3}} = add(Nx.tensor([1, 2, 3]), Nx.tensor(1))
+      assert %Expr{op: :add, args: [_, _], shape: {2, 2}} = add(Nx.tensor([[1, 2], [3, 4]]), Nx.tensor([1, 2]))
+    end
+  end
+
+  describe "aggregate ops" do
+    defn sum_all(t), do: Nx.sum(t)
+    defn sum_1(t), do: Nx.sum(t, axis: 1)
+    defn sum_2(t), do: Nx.sum(t, axes: [0, 1])
+
+    test "to expr" do
+      assert %Expr{op: :sum, args: [_, _], shape: {}} = sum_all(Nx.tensor([1, 2, 3]))
+      assert %Expr{op: :sum, args: [_, _], shape: {2}} = sum_1(Nx.tensor([[1, 2, 3], [1, 2, 3]]))
+      assert %Expr{op: :sum, args: [_, _], shape: {}} = sum_2(Nx.tensor([[1, 2, 3], [1, 2, 3]]))
+    end
+  end
+
+  describe "broadcasting semantics" do
+
+    test "broadcasts scalars" do
+      assert Translation.broadcast({}, {}) == {}
+      assert Translation.broadcast({}, {4, 2, 1, 5}) == {4, 2, 1, 5}
+      assert Translation.broadcast({4, 2, 1, 5}, {}) == {4, 2, 1, 5}
+    end
+
+    test "broadcasts correctly" do
+      assert Translation.broadcast({8, 1, 6, 1}, {7, 1, 5}) == {8, 7, 6, 5}
+      assert Translation.broadcast({7, 1, 5}, {8, 1, 6, 1}) == {8, 7, 6, 5}
+      assert Translation.broadcast({5, 4}, {1}) == {5, 4}
+      assert Translation.broadcast({5, 4}, {4}) == {5, 4}
+      assert Translation.broadcast({15, 3, 5}, {15, 1, 5}) == {15, 3, 5}
+      assert Translation.broadcast({3, 1}, {15, 3, 5}) == {15, 3, 5}
+    end
+
+    test "raises on bad dims" do
+      assert_raise ArgumentError, "could not broadcast shapes because dimensions are" <>
+                                  " incompatible, expected dimensions to be equal or" <>
+                                  " either dimension to be 1, got: 4 and 3", fn ->
+            Translation.broadcast({4, 2, 5}, {3, 2, 5})
+      end
     end
   end
 end
