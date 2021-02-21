1684998ef2a251f52e5e5b73e994b43183b59389
Jason Goldberger <jasongoldberger@gmail.com>
2021-02-21 09:40:44 -0700

    Fix erf_inv examples

--- nx/lib/nx/shared.ex
+++ nx/lib/nx/shared.ex
@@ -255,17 +255,11 @@ defmodule Nx.Shared do
 
   ## Examples
 
-    iex> Nx.erf_inv(0.999)
-    #Nx.Tensor<
-      f64
+      iex> Nx.Shared.erf_inv(0.999)
       2.326753756865462
-    >
 
-    iex> Nx.erf_inv(0.01)
-    #Nx.Tensor<
-      f64
+      iex> Nx.Shared.erf_inv(0.01)
       0.008862500728738846
-    >
   """
   def erf_inv(x) do
     w = -:math.log((1 - x) * (1 + x))
