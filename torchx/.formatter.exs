3df4c7f97b9651fac8d5aaf789bf3c18ae3ea60b
Stas Versilov <stas.versilov@gmail.com>
2021-02-25 18:28:39 +0400

    Import formatter from nx.
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- torchx/.formatter.exs
+++ torchx/.formatter.exs
@@ -1,12 +1,6 @@
 # Used by "mix format"
 
-locals_without_parens = [
-  defn: 2,
-  defnp: 2
-]
-
 [
-  locals_without_parens: locals_without_parens,
-  export: [locals_without_parens: locals_without_parens],
-  inputs: ["{mix,.formatter}.exs", "{lib,test}/**/*.{ex,exs}"]
+  import_deps: [:nx],
+  inputs: ["{mix,.formatter}.exs", "{bench,examples,config,lib,test}/**/*.{ex,exs}"]
 ]
