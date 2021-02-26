3c34b32c360ea940585f7c4533d9601fc3a0a9e1
Stas Versilov <stas.versilov@gmail.com>
2021-02-26 11:42:13 +0400

    Uncomment import

--- torchx/.formatter.exs
+++ torchx/.formatter.exs
@@ -1,6 +1,6 @@
 # Used by "mix format"
 [
   locals_without_parens: [dnif: 1],
-  # import_deps: [:nx],
+  import_deps: [:nx],
   inputs: ["{mix,.formatter}.exs", "{bench,examples,config,lib,test}/**/*.{ex,exs}"]
 ]
