80e26d65f10fecf10d12b67782a22e274581aad3
José Valim <jose.valim@dashbit.co>
2021-02-26 11:26:54 +0100

    Move more AOT configuration upstream

--- exla/lib/exla/aot/compiler.ex
+++ exla/lib/exla/aot/compiler.ex
@@ -12,15 +12,28 @@ defmodule EXLA.AOT.Compiler do
     aot_dir = "aot#{System.unique_integer([:positive])}"
     aot_relative_path = "tensorflow/compiler/xla/exla/#{aot_dir}"
     tf_path = tf_checkout_path()
+    target_triple = target_triple()
+    target_path = "#{module_name}.#{nif_extension(target_triple)}"
 
     config = %{
       aot_dir: aot_dir,
       aot_path: Path.join(tf_path, aot_relative_path),
       aot_relative_path: aot_relative_path,
       lib_name: lib_name,
+      module_name: module_name,
+      target_triple: target_triple(),
+      target_path: target_path,
       tf_path: tf_path
     }
 
+    try do
+      compile(functions, config)
+    after
+      File.rm_rf!(config.aot_path)
+    end
+  end
+
+  defp compile(functions, config) do
     File.rm_rf!(config.aot_path)
     File.mkdir_p!(config.aot_path)
 
@@ -28,36 +41,35 @@ defmodule EXLA.AOT.Compiler do
     functions = Enum.map(functions, &compile_function(&1, config))
 
     # Write out a NIF/BUILD file
-    :ok = write_nif_source_file(functions, module_name, config)
+    :ok = write_nif_source_file(functions, config)
     :ok = write_bazel_build_file(functions, config)
 
-    # Get cwd for output `.so` path
-    cwd = File.cwd!()
-
     # Make an erts symlink
-    erts_path =
-      List.to_string(
-        :lists.concat([:code.root_dir(), '/erts-', :erlang.system_info(:version), '/include'])
-      )
-
+    erts_path = Path.join([:code.root_dir(), "erts-#{:erlang.system_info(:version)}", "include"])
     erts_link_path = Path.join(config.aot_path, "erts")
 
-    with {_, 0} <- System.cmd("ln", ["-s", erts_path, erts_link_path]),
-         {_, 0} <-
-           System.cmd("bazel", ["build", "//#{config.aot_relative_path}:#{lib_name}.so"],
+    case File.ln_s(erts_path, erts_link_path) do
+      :ok -> :ok
+      {:error, _} -> File.cp_r!(erts_path, erts_link_path)
+    end
+
+    case System.cmd("bazel", ["build", "//#{config.aot_relative_path}:#{config.lib_name}.so"],
              cd: config.tf_path,
              stderr_to_stdout: true,
              into: IO.stream(:stdio, :line)
-           ),
-         {_, 0} <-
-           System.cmd("mv", ["bazel-bin/#{config.aot_relative_path}/#{lib_name}.so", cwd],
-             cd: config.tf_path
            ) do
-      File.rm_rf!(config.aot_path)
-    else
-      _ ->
-        File.rm_rf!(config.aot_path)
+      {_, 0} ->
+        so =
+          config.tf_path
+          |> Path.join("bazel-bin/#{config.aot_relative_path}/#{config.lib_name}.so")
+          |> File.read!()
+
+        File.write!(config.target_path, so)
+        :ok
+
+      {_, _} ->
         Logger.error("Unable to complete AOT compilation, something went wrong.")
+        :error
     end
   end
 
@@ -71,7 +83,6 @@ defmodule EXLA.AOT.Compiler do
         Nx.size(shape.dims) * div(size, 8)
       end)
 
-    target_triple = get_target_triple()
     {:ok, pbtext_path} = write_graph_config_file(name, arity, args, sizes, config)
 
     header_path = Path.join(config.aot_path, "#{name}_#{arity}.h")
@@ -84,14 +95,39 @@ defmodule EXLA.AOT.Compiler do
       object_path,
       "#{name}_#{arity}",
       "#{name}_#{arity}_class",
-      target_triple
+      config.target_triple
     )
     |> unwrap!()
 
     {name, arity, args, sizes}
   end
 
-  defp get_target_triple() do
+  defp write_graph_config_file(name, arity, args, sizes, config) do
+    pbtext = Codegen.generate_graph_config_file(args, sizes)
+    pbtext_path = Path.join(config.aot_path, "#{name}_#{arity}.pbtxt")
+    File.write!(pbtext_path, pbtext)
+    {:ok, pbtext_path}
+  end
+
+  defp write_nif_source_file(functions, config) do
+    src = Codegen.generate_nif_source_file(functions, config.module_name, config.aot_relative_path)
+    nif_path = Path.join(config.aot_path, config.lib_name <> ".cc")
+    File.write!(nif_path, src)
+    :ok
+  end
+
+  defp write_bazel_build_file(functions, config) do
+    build = Codegen.generate_bazel_build_file(functions, config.lib_name)
+    build_path = Path.join(config.aot_path, "BUILD")
+    File.write!(build_path, build)
+    :ok
+  end
+
+  defp nif_extension(target_triple) do
+    if String.ends_with?(target_triple, "-windows"), do: :dll, else: :so
+  end
+
+  defp target_triple() do
     # See supported triples: https://github.com/tensorflow/tensorflow/blob/e687cab61615a95b8aea79120c9f168d4cc30955/tensorflow/compiler/aot/tfcompile.bzl
     case :os.type() do
       {:unix, :linux} ->
@@ -105,27 +141,6 @@ defmodule EXLA.AOT.Compiler do
         [arch, vendor | _] = arch_str |> List.to_string() |> String.split("-")
         arch <> "-" <> vendor <> "-" <> Atom.to_string(osname)
     end
-  end
-
-  defp write_graph_config_file(name, arity, args, sizes, config) do
-    pbtext = Codegen.generate_graph_config_file(args, sizes)
-    pbtext_path = Path.join(config.aot_path, "#{name}_#{arity}.pbtxt")
-    File.write!(pbtext_path, pbtext)
-    {:ok, pbtext_path}
-  end
-
-  defp write_nif_source_file(functions, target_module, config) do
-    src = Codegen.generate_nif_source_file(functions, target_module, config.aot_relative_path)
-    nif_path = Path.join(config.aot_path, config.lib_name <> ".cc")
-    File.write!(nif_path, src)
-    :ok
-  end
-
-  defp write_bazel_build_file(functions, config) do
-    build = Codegen.generate_bazel_build_file(functions, config.lib_name)
-    build_path = Path.join(config.aot_path, "BUILD")
-    File.write!(build_path, build)
-    :ok
   end
 
   defp tf_checkout_path() do
