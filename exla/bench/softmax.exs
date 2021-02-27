ed46eecd3797f817174112241d3392d9253f4dc7
José Valim <jose.valim@dashbit.co>
2021-02-27 20:49:55 +0100

    Update benchmarks to use AOT

--- exla/bench/softmax.exs
+++ exla/bench/softmax.exs
@@ -23,14 +23,27 @@ defmodule Softmax do
   defn cuda_keep(n), do: softmax(n)
 end
 
+# Generate a module with an AOT version of softmax.
+Nx.Defn.aot(
+  AOTSoftmax,
+  [
+    {:softmax_64, &Softmax.softmax/1, [t64]},
+    {:softmax_32, &Softmax.softmax/1, [t32]},
+  ],
+  EXLA
+)
+
+IO.inspect(AOTSoftmax.softmax_32(t32))
 IO.inspect(Softmax.softmax(t32))
 IO.inspect(Softmax.host(t32))
 
 benches = %{
   "elixir f32" => fn -> Softmax.softmax(t32) end,
   "elixir f64" => fn -> Softmax.softmax(t64) end,
-  "xla cpu f32" => fn -> Softmax.host(t32) end,
-  "xla cpu f64" => fn -> Softmax.host(t64) end
+  "xla jit-cpu f32" => fn -> Softmax.host(t32) end,
+  "xla jit-cpu f64" => fn -> Softmax.host(t64) end,
+  "xla aot-cpu f32" => fn -> AOTSoftmax.softmax_32(t32) end,
+  "xla aot-cpu f64" => fn -> AOTSoftmax.softmax_64(t64) end
 }
 
 benches =
@@ -39,11 +52,11 @@ benches =
     dt64 = Nx.backend_transfer(t64, EXLA.DeviceBackend, client: :cuda)
 
     Map.merge(benches, %{
-      "xla gpu f32" => fn -> Softmax.cuda(t32) end,
-      "xla gpu f64" => fn -> Softmax.cuda(t64) end,
-      "xla gpu f32 keep" =>
+      "xla jit-gpu f32" => fn -> Softmax.cuda(t32) end,
+      "xla jit-gpu f64" => fn -> Softmax.cuda(t64) end,
+      "xla jit-gpu f32 keep" =>
         {fn -> Softmax.cuda_keep(dt32) end, after_each: &Nx.backend_deallocate/1},
-      "xla gpu f64 keep" =>
+      "xla jit-gpu f64 keep" =>
         {fn -> Softmax.cuda_keep(dt64) end, after_each: &Nx.backend_deallocate/1}
     })
   else
