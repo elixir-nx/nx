31d4406902a94ff14d037b62bd4bca9b489d928f
Sean Moriarity <smoriarity.5@gmail.com>
2021-03-01 15:07:14 -0500

    Update nx/lib/nx/defn/grad.ex
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- nx/lib/nx/defn/grad.ex
+++ nx/lib/nx/defn/grad.ex
@@ -471,6 +471,7 @@ defmodule Nx.Defn.Grad do
     rhs_dilated_shape = Tuple.to_list(Nx.Shape.pad(rhs_sdims, rhs_dilated_padding_config))
     out_dilated_shape = Tuple.to_list(Nx.Shape.pad(out_sdims, out_dilated_padding_config))
 
+    # TODO: Use Enum.zip_with on Elixir v1.12
     pad_before =
       rhs_dilated_shape
       |> Enum.zip(padding)
