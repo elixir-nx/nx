b71e392018140940c0e38bffc0509de2b321fe8a
Stas Versilov <stas.versilov@gmail.com>
2021-03-12 13:16:59 +0400

    CUDA availability check

--- torchx/lib/torchx.ex
+++ torchx/lib/torchx.ex
@@ -6,9 +6,8 @@ defmodule Torchx do
   Device atom can be any of
   [:cpu, :cuda, :mkldnn, :opengl, :opencl, :ideep, :hip, :fpga, :msnpu, :xla, :vulkan, :metal, :xpu].
 
-  But only :cuda is supported for now.
+  But only :cuda availability check is supported for now.
   """
-
   def device_available?(:cuda), do: NIF.cuda_is_available()
   def device_available?(:cpu), do: true
   def device_available?(_), do: raise("Only CUDA device availability check is supported for now.")
