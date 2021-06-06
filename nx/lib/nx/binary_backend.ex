a27c010e473b9d51b6a6df4edd2171f1c19c1d0f
Sean Moriarity <smoriarity.5@gmail.com>
2021-06-06 11:51:27 -0400

    Update nx/lib/nx/binary_backend.ex
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- nx/lib/nx/binary_backend.ex
+++ nx/lib/nx/binary_backend.ex
@@ -2,6 +2,7 @@ defmodule Nx.BinaryBackend do
   @moduledoc """
   An opaque backend written in pure Elixir that stores
   the data in Elixir's binaries.
+
   This is the default backend used by the `Nx` module.
   The backend itself (and its data) is private and must
   not be accessed directly.
