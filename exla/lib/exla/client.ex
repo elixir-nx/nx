d691b71ddcf4858a2791e5ac3c890be853b23afd
José Valim <jose.valim@dashbit.co>
2021-07-08 15:48:22 +0200

    Use ETS for locked cache

--- exla/lib/exla/client.ex
+++ exla/lib/exla/client.ex
@@ -1,11 +1,12 @@
 defmodule EXLA.Client do
   @moduledoc """
-  Functions for managing `Exla.Client`.
+  Functions for managing `EXLA.Client`.
 
-  See `Exla` module docs for a general introduction.
+  See `EXLA` module docs for a general introduction.
   """
 
-  alias __MODULE__
+  use GenServer
+  @name __MODULE__
 
   @enforce_keys [:ref, :platform, :name, :device_count, :devices]
   defstruct [:ref, :platform, :name, :device_count, :devices]
@@ -14,50 +15,18 @@ defmodule EXLA.Client do
   Fetches a client with the given `name` from configuration.
   """
   def fetch!(name) do
-    {_, client} = fetch_client!(name)
-    client
-  end
+    :persistent_term.get({__MODULE__, name}, nil) ||
+      (
+        clients = Application.fetch_env!(:exla, :clients)
 
-  defp fetch_client!(name) do
-    EXLA.LockedCache.run({__MODULE__, name}, fn ->
-      clients = Application.fetch_env!(:exla, :clients)
+        options =
+          Keyword.get(clients, name) ||
+            raise ArgumentError,
+                  "could not find EXLA client named #{inspect(name)}, the clients specified " <>
+                    "in your config files are: #{inspect(Keyword.keys(clients))}"
 
-      options =
-        Keyword.get(clients, name) ||
-          raise ArgumentError,
-                "could not find EXLA client named #{inspect(name)}, the clients specified " <>
-                  "in your config files are: #{inspect(Keyword.keys(clients))}"
-
-      platform = Keyword.get(options, :platform, :host)
-      # Fraction of GPU memory to preallocate
-      memory_fraction = Keyword.get(options, :memory_fraction, 0.9)
-      # Flag for preallocating GPU memory
-      preallocate = Keyword.get(options, :preallocate, true)
-      preallocate_int = if preallocate, do: 1, else: 0
-
-      ref =
-        case platform do
-          :host -> EXLA.NIF.get_host_client()
-          :cuda -> EXLA.NIF.get_gpu_client(memory_fraction, preallocate_int)
-          :rocm -> EXLA.NIF.get_gpu_client(memory_fraction, preallocate_int)
-          :tpu -> EXLA.NIF.get_tpu_client()
-          _ -> raise ArgumentError, "unknown Exla platform: #{inspect(platform)}"
-        end
-        |> unwrap!()
-
-      device_count = EXLA.NIF.get_device_count(ref) |> unwrap!()
-      devices = EXLA.NIF.get_devices(ref) |> unwrap!()
-
-      client = %Client{
-        ref: ref,
-        platform: platform,
-        name: name,
-        device_count: device_count,
-        devices: devices
-      }
-
-      {nil, client}
-    end)
+        GenServer.call(@name, {:client, name, options}, :infinity)
+      )
   end
 
   @doc """
@@ -65,6 +34,57 @@ defmodule EXLA.Client do
   """
   def get_supported_platforms do
     EXLA.NIF.get_supported_platforms() |> unwrap!()
+  end
+
+  ## Callbacks
+
+  @doc false
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  @doc false
+  def init(:ok) do
+    {:ok, :unused_state}
+  end
+
+  @doc false
+  def handle_call({:client, name, options}, _from, state) do
+    client = :persistent_term.get({__MODULE__, name}, nil) || build_client(name, options)
+    :persistent_term.put({__MODULE__, name}, client)
+    {:reply, client, state}
+  end
+
+  defp build_client(name, options) do
+    platform = Keyword.get(options, :platform, :host)
+
+    # Fraction of GPU memory to preallocate
+    memory_fraction = Keyword.get(options, :memory_fraction, 0.9)
+
+    # Flag for preallocating GPU memory
+    preallocate = Keyword.get(options, :preallocate, true)
+    preallocate_int = if preallocate, do: 1, else: 0
+
+    ref =
+      case platform do
+        :host -> EXLA.NIF.get_host_client()
+        :cuda -> EXLA.NIF.get_gpu_client(memory_fraction, preallocate_int)
+        :rocm -> EXLA.NIF.get_gpu_client(memory_fraction, preallocate_int)
+        :tpu -> EXLA.NIF.get_tpu_client()
+        _ -> raise ArgumentError, "unknown Exla platform: #{inspect(platform)}"
+      end
+      |> unwrap!()
+
+    device_count = EXLA.NIF.get_device_count(ref) |> unwrap!()
+    devices = EXLA.NIF.get_devices(ref) |> unwrap!()
+
+    %EXLA.Client{
+      ref: ref,
+      platform: platform,
+      name: name,
+      device_count: device_count,
+      devices: devices
+    }
   end
 
   defp unwrap!({:ok, ref}), do: ref
