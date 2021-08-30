12d34a6789853bb2adcba3fc6ee42c3c9500e722
José Valim <jose.valim@dashbit.co>
2021-08-30 08:32:21 +0200

    Clarify backend in error message

--- nx/lib/nx/binary_backend/matrix.ex
+++ nx/lib/nx/binary_backend/matrix.ex
@@ -366,11 +366,11 @@ defmodule Nx.BinaryBackend.Matrix do
 
     {s, [vt_row | _] = vt} = apply_singular_value_corrections(s, vt)
 
-    # TO-DO: complete the vt matrix with linearly independent rows
+    # TODO: complete the vt matrix with linearly independent rows
     # requires solving a homogeneous equation system for non-homogenous
     # solutions
     if length(vt) != vt_rows or length(vt_row) != vt_cols do
-      raise "vt matrix completion for wide-matrices not implemented"
+      raise "vt matrix completion for wide-matrices not implemented for Nx.BinaryBackend"
     end
 
     {u |> approximate_zeros(eps) |> matrix_to_binary(output_type),
