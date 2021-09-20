d1650d24a20cca87e64d2e2a33ea742f7db225b9
José Valim <jose.valim@dashbit.co>
2021-09-20 17:57:53 +0200

    More defn tests

--- torchx/test/torchx/defn_test.exs
+++ torchx/test/torchx/defn_test.exs
@@ -2,10 +2,21 @@ defmodule Torchx.DefnTest do
   use ExUnit.Case, async: true
 
   import Nx.Defn
+  alias Nx.Tensor, as: T
+  alias Torchx.Backend, as: TB
 
   setup do
     Nx.default_backend(Torchx.Backend)
     :ok
+  end
+
+  describe "creation" do
+    defn iota, do: Nx.iota({10, 10})
+
+    test "iota" do
+      %T{data: %TB{}} = tensor = iota()
+      assert Nx.backend_transfer(tensor) == Nx.iota({10, 10}, backend: Nx.BinaryBackend)
+    end
   end
 
   describe "scalar" do
@@ -13,14 +24,20 @@ defmodule Torchx.DefnTest do
     defn integer_scalar(x), do: Nx.add(1, x)
     defn broadcast_scalar(x), do: Nx.add(Nx.broadcast(1, {2, 2}), x)
 
-    test "works" do
-      assert float_scalar(0.0) |> Nx.backend_transfer() ==
-               Nx.tensor(1.0, backend: Nx.BinaryBackend)
+    test "float" do
+      %T{data: %TB{}} = tensor = float_scalar(0.0)
+      assert Nx.backend_transfer(tensor) == Nx.tensor(1.0, backend: Nx.BinaryBackend)
+    end
 
-      assert integer_scalar(0) |> Nx.backend_transfer() ==
-               Nx.tensor(1, backend: Nx.BinaryBackend)
+    test "integer" do
+      %T{data: %TB{}} = tensor = integer_scalar(0)
+      assert Nx.backend_transfer(tensor) == Nx.tensor(1, backend: Nx.BinaryBackend)
+    end
 
-      assert broadcast_scalar(0) |> Nx.backend_transfer() ==
+    test "broadcast" do
+      %T{data: %TB{}} = tensor = broadcast_scalar(0)
+
+      assert Nx.backend_transfer(tensor) ==
                Nx.broadcast(Nx.tensor(1, backend: Nx.BinaryBackend), {2, 2})
     end
   end
