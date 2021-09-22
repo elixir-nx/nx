651269681a178e02996b499b4c571920a8074bbe
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2021-09-22 09:43:49 -0300

    docs: remove extra docs

--- torchx/test/torchx/nx_doctest_test.exs
+++ torchx/test/torchx/nx_doctest_test.exs
@@ -4,57 +4,8 @@ defmodule Torchx.NxDoctestTest do
 
   Many tests are excluded for the reasons below, coverage
   for the excluded tests can be found on Torchx.BackendTest.
-
-  ## Not callbacks
-
-    * default_backend/1
-
-  ## PyTorch rounds differently than Elixir
-
-    * round/1
-
-  ## Result mismatch in less significant digits (precision issue)
-
-    * logistic/1
-    * cos/1
-    * erfc/1
-
-  ## Callback or test requires explicit backend transfer to BinaryBackend
-
-    * to_binary/2
-    * to_flat_list/2
-    * random_uniform/4
-    * random_normal/4
-    * atan2/2
-
-  ## Not all options are supported
-
-    * argmax/2
-    * argmin/2
-    * to_batched_list/3
-
-  ## Unsigned ints not supported by Torch
-
-    * quotient/2
-    * mean/2
-
-  ## Output mismatch
-
-    * qr/2
-    * cholesky/1
-
-  ## Not implemented yet or depends on not implemented
-
-    * window_mean/3
-    * window_sum/3
-    * map/3
-    * norm/2
-    * all_close?/3
-    * bitcast/2
-
   """
 
-  # TODO: Move these explanations alongside the functions below
   # TODO: Add backend tests for the doctests that are excluded
 
   use ExUnit.Case, async: true
