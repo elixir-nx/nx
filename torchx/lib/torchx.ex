e160765b779123663a85b951c8e49f2c640bd2bb
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2021-10-04 21:33:51 -0300

    chore: remove take from torchx.ex

--- torchx/lib/torchx.ex
+++ torchx/lib/torchx.ex
@@ -164,7 +164,6 @@ defmodule Torchx do
   deftensor gather(tensor_input, tensor_indices, tensor_linear_indices_offsets, output_shape)
   deftensor take_along_axis(tensor_input, tensor_indices, axis)
   deftensor argsort(tensor, axis, is_descending)
-  deftensor take(tensor, tensor_indices, axis)
 
   ## Aggregation
 
