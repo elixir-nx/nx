4b38c606fe38417284e6bf32278b3df86f01543d
Sean Moriarity <smoriarity.5@gmail.com>
2021-10-19 08:03:54 -0400

    Add set_supported_platforms helper (#522)

--- exla/lib/exla/client.ex
+++ exla/lib/exla/client.ex
@@ -33,7 +33,56 @@ defmodule EXLA.Client do
   Returns a map of supported platforms with device information.
   """
   def get_supported_platforms do
-    EXLA.NIF.get_supported_platforms() |> unwrap!()
+    EXLA.NIF.get_supported_platforms()
+    |> unwrap!()
+    |> Map.new(fn {k, v} ->
+      k = k |> List.to_string() |> String.downcase(:ascii) |> String.to_atom()
+      {k, v}
+    end)
+  end
+
+  @doc """
+  Automatically sets platform of given client based on available platforms
+  and given platform precedence.
+
+  *NOTE*: This sets application configuration and must run before using any
+  EXLA functionality.
+
+  ## Examples
+
+  This is typically helpful when writing scripts which could potentially be executed
+  from multiple platforms:
+
+      EXLA.set_preferred_platform(:default, [:tpu, :cuda, :rocm, :host])
+
+  The above will try to set the default client platform in order from `:tpu` to`:host`,
+  halting when it finds the highest precedence supported platform. EXLA falls back to
+  `:host` when there is no platform specified for the given client, so you can omit
+  `:host` if it's last in precedence:
+
+      # Falls back to :host if :cuda is not available
+      EXLA.set_preferred_platform(:default, [:cuda])
+
+  You can set a preferred platform for multiple clients:
+
+      # :rocm client prefers :rocm platform
+      EXLA.set_preferred_platform(:rocm, [:rocm])
+
+      # :default client prefers :tpu then :cuda
+      EXLA.set_preferred_platform(:default, [:tpu, :cuda])
+  """
+  def set_preferred_platform(client_name, order) do
+    supported_platforms = get_supported_platforms()
+
+    order
+    |> Enum.reduce_while(:ok, fn platform, :ok ->
+      if Map.has_key?(supported_platforms, platform) do
+        Application.put_env(:exla, :clients, {client_name, [platform: platform]})
+        {:halt, :ok}
+      else
+        {:cont, :ok}
+      end
+    end)
   end
 
   ## Callbacks
