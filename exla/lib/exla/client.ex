2fc3c41c4422c205ccd5e247a39f3b7eac63adbc
Sean Moriarity <smoriarity.5@gmail.com>
2021-10-19 08:01:46 -0400

    Suggestions from code review

--- exla/lib/exla/client.ex
+++ exla/lib/exla/client.ex
@@ -36,7 +36,7 @@ defmodule EXLA.Client do
     EXLA.NIF.get_supported_platforms()
     |> unwrap!()
     |> Map.new(fn {k, v} ->
-      k = k |> List.to_string() |> String.downcase() |> String.to_atom()
+      k = k |> List.to_string() |> String.downcase(:ascii) |> String.to_atom()
       {k, v}
     end)
   end
@@ -44,8 +44,34 @@ defmodule EXLA.Client do
   @doc """
   Automatically sets platform of given client based on available platforms
   and given platform precedence.
+
+  *NOTE*: This sets application configuration and must run before using any
+  EXLA functionality.
+
+  ## Examples
+
+  This is typically helpful when writing scripts which could potentially be executed
+  from multiple platforms:
+
+      EXLA.set_preferred_platform(:default, [:tpu, :cuda, :rocm, :host])
+
+  The above will try to set the default client platform in order from `:tpu` to`:host`,
+  halting when it finds the highest precedence supported platform. EXLA falls back to
+  `:host` when there is no platform specified for the given client, so you can omit
+  `:host` if it's last in precedence:
+
+      # Falls back to :host if :cuda is not available
+      EXLA.set_preferred_platform(:default, [:cuda])
+
+  You can set a preferred platform for multiple clients:
+
+      # :rocm client prefers :rocm platform
+      EXLA.set_preferred_platform(:rocm, [:rocm])
+
+      # :default client prefers :tpu then :cuda
+      EXLA.set_preferred_platform(:default, [:tpu, :cuda])
   """
-  def set_preferred_platforms(client_name, order) do
+  def set_preferred_platform(client_name, order) do
     supported_platforms = get_supported_platforms()
 
     order
