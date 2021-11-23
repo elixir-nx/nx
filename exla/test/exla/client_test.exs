adda98df8dd2479a5d2adf42bbb7b8368ecd1d13
José Valim <jose.valim@dashbit.co>
2021-11-23 08:02:23 +0100

    Add missing client test

---
+++ exla/test/exla/client_test.exs
@@ -0,0 +1,11 @@
+defmodule EXLA.CLientTest do
+  use ExUnit.Case, async: true
+
+  doctest EXLA.Client
+
+  describe "get_supported_platforms/0" do
+    test "returns supported platforms with device information" do
+      %{host: _} = EXLA.Client.get_supported_platforms()
+    end
+  end
+end
