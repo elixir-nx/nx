09384d7059a75c97783f5cd9d5e9f56459cd860a
José Valim <jose.valim@dashbit.co>
2021-11-29 19:00:12 +0100

    Remove brackets from tensor expressions
    They are not necessary and therefore become noise.

--- nx/lib/nx/defn/expr.ex
+++ nx/lib/nx/defn/expr.ex
@@ -1007,7 +1007,7 @@ defmodule Nx.Defn.Expr do
 
     all
     |> Enum.map(fn {str, tensor} ->
-      String.pad_trailing(str, length, " ") <> "  " <> to_type_shape(tensor)
+      String.pad_trailing(str, length, " ") <> "   " <> to_type_shape(tensor)
     end)
     |> Enum.uniq()
     |> Enum.reduce(header, &concat(&2, concat(line(), &1)))
@@ -1040,7 +1040,7 @@ defmodule Nx.Defn.Expr do
     {args, {exprs, params, var_map, cache}} = traverse_args(op, t, acc)
     {var, var_map} = var_for_id(var_map, id)
     args_str = inspect_args(op, args, var_map)
-    expr_str = var <> " = " <> Atom.to_string(op) <> " [ " <> args_str <> " ]"
+    expr_str = var <> " = " <> Atom.to_string(op) <> " " <> args_str
     {t, {[{expr_str, t} | exprs], params, var_map, cache}}
   end
 
