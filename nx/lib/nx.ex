3ed62293470d291ab2f11ee1efec15c83fa4eff2
float2 <nicholas.cheredov@gmail.com>
2021-12-06 21:19:41 +0300

    Add: Nx.to_number/1

--- nx/lib/nx.ex
+++ nx/lib/nx.ex
@@ -1574,6 +1574,38 @@ defmodule Nx do
   end
 
   @doc """
+  Returns the underlying tensor as a number.
+
+  If the tensor has a dimension, it raises.
+
+  ## Examples
+
+      iex> Nx.to_number(1)
+      1
+
+      iex> Nx.to_number(Nx.tensor([1.0, 2.0, 3.0]))
+      ** (ArgumentError) cannot convert tensor of shape {3} to a number
+
+  """
+  @doc type: :conversion
+  def to_number(tensor)
+
+  def to_number(number) when is_number(number), do: number
+
+  def to_number(tensor) do
+    tensor = to_tensor(tensor)
+
+    if tensor.shape != {} do
+      raise ArgumentError, "cannot convert tensor of shape #{inspect(tensor.shape)} to number"
+    end
+
+    match_types [tensor.type] do
+      <<match!(x, 0)>> = to_binary(tensor)
+      read!(x, 0)
+    end
+  end
+
+  @doc """
   Returns the underlying tensor as a scalar.
 
   If the tensor has a dimension, it raises.
