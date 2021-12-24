790b355523351859a496e4114aedf31824a756f0
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2021-12-24 09:58:51 -0300

    fix(torchx): grow start index when length goes out of bounds (#594)
    * fix(torchx): grow start index when length goes out of bounds
    
    * refactor: use binary backend as dynamic reference for test

--- torchx/test/torchx_test.exs
+++ torchx/test/torchx_test.exs
@@ -33,4 +33,18 @@ defmodule TorchxTest do
       assert Nx.to_binary(tensor) == tensor |> Torchx.from_nx() |> Torchx.to_blob()
     end
   end
+
+  describe "slice/4" do
+    test "out of bound indices" do
+      tensor = Nx.iota({6, 5, 4})
+
+      slice = fn t -> Nx.slice(t, [1, 1, 1], [6, 5, 4], strides: [2, 3, 1]) end
+
+      expected = tensor |> Nx.backend_transfer(Nx.BinaryBackend) |> then(slice)
+
+      result = slice.(tensor)
+
+      assert expected |> Nx.equal(result) |> Nx.all?() |> Nx.to_number() == 1
+    end
+  end
 end
