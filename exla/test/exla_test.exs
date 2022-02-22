03f3074da0d9b44ba17c1b306c07b0078fea01a2
José Valim <jose.valim@dashbit.co>
2022-02-22 11:53:41 +0100

    Add EXLA.set_as_nx_default/2 (#659)

--- exla/test/exla_test.exs
+++ exla/test/exla_test.exs
@@ -8,16 +8,16 @@ defmodule EXLA.GlobalTest do
 
   @default Nx.Defn.global_default_options([])
 
-  test "set_preferred_defn_options" do
+  test "set_as_nx_default" do
     assert @default[:compiler] == EXLA
 
     Nx.Defn.global_default_options([])
     assert Nx.Defn.default_options() == []
 
-    assert EXLA.set_preferred_defn_options([:unknown]) == nil
+    assert EXLA.set_as_nx_default([:unknown]) == nil
     assert Nx.Defn.default_options() == []
 
-    assert EXLA.set_preferred_defn_options([:host]) == :host
+    assert EXLA.set_as_nx_default([:host]) == :host
     assert Nx.Defn.default_options() == [compiler: EXLA, client: :host]
   after
     assert Nx.Defn.global_default_options(@default)
