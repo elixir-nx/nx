57660d72f68538ad9e484e84e1f5266cf5ec0d3f
José Valim <jose.valim@dashbit.co>
2022-02-23 16:56:02 +0100

    Teach jit/stream to work on collections by default (#663)

--- nx/test/support/identity.ex
+++ nx/test/support/identity.ex
@@ -1,10 +1,11 @@
 defmodule Nx.Defn.Identity do
   @behaviour Nx.Defn.Compiler
 
-  def __stream__(_, _, _, _, _, _), do: raise("not implemented")
+  def __stream__(_, _, _, _, _, _, _), do: raise("not implemented")
 
-  def __jit__(key, vars, fun, _opts) do
+  def __jit__(key, vars, fun, args, _opts) do
     Process.put(__MODULE__, key)
-    fun.(vars)
+    expr = fun.(vars)
+    Enum.map(args, fn _ -> expr end)
   end
 end
