c928527cc1ec3f8d9f794fb33fcb062e826eb778
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-04-10 03:05:49 -0300

    fix: add missing atan2 clause

--- torchx/lib/torchx/backend.ex
+++ torchx/lib/torchx/backend.ex
@@ -708,6 +708,13 @@ defmodule Torchx.Backend do
     raise ArithmeticError, "Torchx does not support complex values for atan2"
   end
 
+  def atan2(out, left, right) do
+    left
+    |> from_nx()
+    |> Torchx.atan2(from_nx(right))
+    |> to_nx(out)
+  end
+
   binary_ops =
     [:add, :subtract, :multiply, :power, :remainder, :divide, :min, :max, :quotient] ++
       [:left_shift, :right_shift] ++
