2daac973c959ba2025b48783cffba4ae39f5edfb
Sean Moriarity <smoriarity.5@gmail.com>
2022-05-11 17:46:31 -0400

    Add block host on destructor (#780)
    * Add block host on destructor
    
    * Update exla/c_src/exla/exla_client.h
    
    Co-authored-by: José Valim <jose.valim@dashbit.co>
    
    * Update exla/c_src/exla/exla_client.h
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>
    
    Co-authored-by: José Valim <jose.valim@dashbit.co>
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- exla/c_src/exla/exla_client.h
+++ exla/c_src/exla/exla_client.h
@@ -25,6 +25,11 @@ class ExlaBuffer {
   ExlaBuffer(std::unique_ptr<xla::PjRtBuffer> buffer,
              bool can_be_released_after_run_ = false);
 
+  // Make sure the transfer has completed before we deallocate the buffer.
+  ~ExlaBuffer() {
+    buffer_->BlockHostUntilReady();
+  }
+
   bool release_after_run() { return can_be_released_after_run_; }
   xla::PjRtBuffer* buffer() { return buffer_.get(); }
   xla::StatusOr<ExlaBuffer*> CopyToDevice(xla::PjRtDevice * dst_device);
