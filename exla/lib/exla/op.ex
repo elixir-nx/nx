f76bbb970b712051a6ce7e8c31faf587825c9ed0
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-07-19 17:02:13 -0300

    refactor: reuse code for is_inf and is_nan

--- exla/lib/exla/op.ex
+++ exla/lib/exla/op.ex
@@ -189,46 +189,28 @@ defmodule EXLA.Op do
     %{op | ref: ref}
   end
 
-  def is_nan(%{ref: ref} = op, {:c, _}, _shape, _axes, _state) do
-    re_part = ref |> EXLA.NIF.real() |> unwrap!() |> EXLA.NIF.is_nan() |> unwrap!()
-    im_part = ref |> EXLA.NIF.imag() |> unwrap!() |> EXLA.NIF.is_nan() |> unwrap!()
+  def is_nan(op, type, shape, axes, state),
+    do: is_non_finite(&EXLA.NIF.is_nan/1, op, type, shape, axes, state)
+
+  def is_infinity(op, type, shape, axes, state),
+    do: is_non_finite(&EXLA.NIF.is_infinity/1, op, type, shape, axes, state)
+
+  def is_non_finite(nif_function, %{ref: ref} = op, {:c, _}, _shape, _axes, _state) do
+    re_part = ref |> EXLA.NIF.real() |> unwrap!() |> nif_function.() |> unwrap!()
+    im_part = ref |> EXLA.NIF.imag() |> unwrap!() |> nif_function.() |> unwrap!()
 
     result_ref = EXLA.NIF.bitwise_or(re_part, im_part, {}) |> unwrap!()
 
     %{op | ref: result_ref}
   end
 
-  def is_nan(op, {t, _}, _shape, _axes, _state) when t in [:f, :bf] do
+  def is_non_finite(nif_function, op, {t, _}, _shape, _axes, _state) when t in [:f, :bf] do
     %{ref: ref} = op
-    result_ref = EXLA.NIF.is_nan(ref) |> unwrap!()
+    result_ref = nif_function.(ref) |> unwrap!()
     %{op | ref: result_ref}
   end
 
-  def is_nan(_op, _type, shape, axes, %{builder: builder}) do
-    # For non-floating types, we can just return
-    # a boolean 0 tensor in the output shape
-    builder
-    |> constant_r0(0, {:u, 8})
-    |> reshape(Tuple.duplicate(1, tuple_size(shape)))
-    |> broadcast_in_dim(shape, List.to_tuple(axes))
-  end
-
-  def is_infinity(%{ref: ref} = op, {:c, _}, _shape, _axes, _state) do
-    re_part = ref |> EXLA.NIF.real() |> unwrap!() |> EXLA.NIF.is_infinity() |> unwrap!()
-    im_part = ref |> EXLA.NIF.imag() |> unwrap!() |> EXLA.NIF.is_infinity() |> unwrap!()
-
-    result_ref = EXLA.NIF.bitwise_or(re_part, im_part, {}) |> unwrap!()
-
-    %{op | ref: result_ref}
-  end
-
-  def is_infinity(op, {t, _}, _shape, _axes, _state) when t in [:f, :bf] do
-    %{ref: ref} = op
-    result_ref = EXLA.NIF.is_infinity(ref) |> unwrap!()
-    %{op | ref: result_ref}
-  end
-
-  def is_infinity(_op, _type, shape, axes, %{builder: builder}) do
+  def is_non_finite(_nif_function, _op, _type, shape, axes, %{builder: builder}) do
     # For non-floating types, we can just return
     # a boolean 0 tensor in the output shape
     builder
