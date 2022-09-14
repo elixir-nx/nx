4fd47047d2f365d5e79ef6b8f255b62ee1b614bc
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-09-14 17:50:00 -0300

    Update torchx/test/torchx/nx_doctest_test.exs
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- torchx/test/torchx/nx_doctest_test.exs
+++ torchx/test/torchx/nx_doctest_test.exs
@@ -15,7 +15,6 @@ defmodule Torchx.NxDoctestTest do
 
   @temporarily_broken_doctests [
     # broadcast - shape mismatch in one test
-    # broadcast: 3,
     # mean - Torchx does not support unsigned 64 bit integer
     mean: 2,
     # quotient - Torchx does not support unsigned 32 bit integer
