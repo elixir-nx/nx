26cb52ffd6b200fc4aef178e6f1ae27a792ef584
José Valim <jose.valim@dashbit.co>
2022-10-16 12:05:44 +0200

    Further simplify random to use access instead of padding

--- nx/lib/nx/random.ex
+++ nx/lib/nx/random.ex
@@ -69,7 +69,7 @@ defmodule Nx.Random do
     k2 = Nx.bitwise_and(seed, 0xFFFFFFFF)
 
     Nx.stack([k1, k2])
-    |> Nx.as_type({:u, 32})
+    |> Nx.as_type(:u32)
   end
 
   @doc """
@@ -102,33 +102,29 @@ defmodule Nx.Random do
   defn split(key, num \\ 2) do
     assert_key!(key)
 
-    Nx.iota({2, num}, type: :u32)
-    |> threefry2x32_20(key)
-    |> Nx.reshape({num, 2})
+    threefry2x32(key, {num, 2})
   end
 
-  defnp threefry2x32(key, count) do
-    case count |> Nx.size() |> rem(2) do
+  defnp threefry2x32(key, shape) do
+    case shape |> Nx.size() |> rem(2) do
       0 ->
-        count
-        |> Nx.reshape({2, :auto})
+        Nx.iota({2, div(Nx.size(shape), 2)}, type: :u32)
         |> threefry2x32_20(key)
-        |> Nx.reshape(count)
+        |> Nx.reshape(shape)
 
       1 ->
-        Nx.flatten(count)
-        |> Nx.pad(0, [{0, 1, 0}])
+        Nx.concatenate([Nx.iota({Nx.size(shape)}, type: :u32), Nx.tensor([0], type: :u32)])
         |> Nx.reshape({2, :auto})
         |> threefry2x32_20(key)
         |> Nx.flatten()
-        |> Nx.pad(0, [{0, -1, 0}])
-        |> Nx.reshape(count)
+        |> Access.get(0..-2//1)
+        |> Nx.reshape(shape)
     end
   end
 
   defnp threefry2x32_20(xs, ks) do
     rotations =
-      {Nx.tensor([13, 15, 26, 6], type: {:u, 8}), Nx.tensor([17, 29, 16, 24], type: {:u, 8})}
+      {Nx.tensor([13, 15, 26, 6], type: :u8), Nx.tensor([17, 29, 16, 24], type: :u8)}
 
     key1 = ks[0]
     key2 = ks[1]
@@ -193,18 +189,17 @@ defmodule Nx.Random do
     case bit_width do
       64 ->
         bits =
-          threefry2x32(key, Nx.iota({Nx.size(shape) * 2}, type: {:u, 32}))
-          |> Nx.reshape({2, :auto})
+          threefry2x32(key, {2, Nx.size(shape)})
           |> Nx.as_type({:u, 64})
 
         bits = bits[0] <<< 32 ||| bits[1]
         Nx.reshape(bits, shape)
 
       32 ->
-        threefry2x32(key, Nx.iota(shape, type: {:u, 32}))
+        threefry2x32(key, shape)
 
       _ ->
-        threefry2x32(key, Nx.iota(shape, type: {:u, 32}))
+        threefry2x32(key, shape)
         |> Nx.as_type({:u, bit_width})
     end
   end
