c151893366c3fc7c0e77ed14dba0878ff9531c89
José Valim <jose.valim@dashbit.co>
2022-12-03 18:13:37 +0100

    Prepare for sync EXLA.Backend operations

--- exla/lib/exla.ex
+++ exla/lib/exla.ex
@@ -30,11 +30,6 @@ defmodule EXLA do
   `EXLA.Backend`. You can use functions such as `Nx.backend_transfer/2` to
   explicitly transfer tensors.
 
-  Note that the `EXLA.Backend` is asynchronous: operations on its tensors
-  *may* return immediately, before the tensor data is available. The backend
-  will then block only when trying to read the data or when passing it to
-  another operation.
-
   EXLA will pick an available client to allocate and compute tensors, in this
   order: `:cuda`, `:rocm`, `:tpu`, and `:host` (CPU). See the "Clients" section
   below for more information.
