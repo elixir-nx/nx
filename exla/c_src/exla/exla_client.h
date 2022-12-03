33f6e2455790024e70fe85da2a1a33f4f7b19bf1
Sean Moriarity <smoriarity.5@gmail.com>
2022-12-03 09:14:22 -0800

    Update exla/c_src/exla/exla_client.h
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- exla/c_src/exla/exla_client.h
+++ exla/c_src/exla/exla_client.h
@@ -31,11 +31,8 @@ class ExlaBuffer {
   xla::Status Deallocate();
 
   ~ExlaBuffer() {
-    // TODO: We likely want to keep the buffer as a shared pointer
-    // between Erlang VM and XLA and use AcquireExternalReference
-    // to notify that the buffer should be kept around until the
-    // reference is released.
-    // https://github.com/tensorflow/tensorflow/blob/b8eb820d6cb27cfa8ab65c40ce9a161de314533c/tensorflow/compiler/xla/pjrt/pjrt_client.h#L763-L780
+    // Theoretically this may block if a computation is running
+    // but we always block the host until the computation is done.
   }
 
  private:
