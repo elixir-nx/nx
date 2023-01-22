e716326f2ecd100f39407aa00ce7f3db4cac5c48
José Valim <jose.valim@gmail.com>
2023-01-22 18:19:25 +0100

    Update nx/lib/nx/lin_alg.ex

--- nx/lib/nx/lin_alg.ex
+++ nx/lib/nx/lin_alg.ex
@@ -1763,9 +1763,14 @@ defmodule Nx.LinAlg do
   @doc """
   Return matrix rank of input M × N matrix using Singular Value Decomposition method.
 
-  Approximate the number of linearly independent rows by calculating the number of singular values greater than `eps * max(singular values) * max(M, N)`.
-  This also appears in Numerical recipes in the discussion of SVD solutions for linear least squares [1].
-  [1] W. H. Press, S. A. Teukolsky, W. T. Vetterling and B. P. Flannery, “Numerical Recipes (3rd edition)”, Cambridge University Press, 2007, page 795.
+  Approximate the number of linearly independent rows by calculating the number
+  of singular values greater than `eps * max(singular values) * max(M, N)`.
+
+  This also appears in Numerical recipes in the discussion of SVD solutions for
+  linear least squares [1].
+
+  [1] W. H. Press, S. A. Teukolsky, W. T. Vetterling and B. P. Flannery,
+  “Numerical Recipes (3rd edition)”, Cambridge University Press, 2007, page 795.
 
   ## Options
     * `:eps` - Rounding error threshold used to assume values as 0. Defaults to `1.0e-10`
