3a6ad45a676e78dd4aebaaf6b87b808558713942
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-03-10 12:17:06 -0300

    feat: make vectorized work with exla

--- exla/test/exla/vectorize_test.exs
+++ exla/test/exla/vectorize_test.exs
@@ -3,28 +3,38 @@ defmodule Nx.VectorizeTest do
 
   import Nx.Defn
 
-  @base Nx.tensor([
+  setup do
+    backend = Nx.default_backend(EXLA.Backend)
+
+    base =
+      Nx.tensor(
+        [
           [[0, 1, 2]],
           [[3, 4, 5]],
           [[6, 7, 8]]
-        ])
+        ],
+        backend: EXLA.Backend
+      )
 
-  @vectorized Nx.vectorize(@base, :rows)
+    vectorized = Nx.vectorize(base, :rows)
 
-  @base_math Nx.tensor([
-               [[0.1, 0.2, 0.3]],
-               [[0.4, 0.5, 0.6]],
-               [[0.7, 0.8, 0.9]]
-             ])
+    base_math =
+      Nx.tensor(
+        [
+          [[0.1, 0.2, 0.3]],
+          [[0.4, 0.5, 0.6]],
+          [[0.7, 0.8, 0.9]]
+        ],
+        backend: EXLA.Backend
+      )
 
-  @vectorized_math Nx.vectorize(@base_math, :rows)
-
-  setup do
-    backend = Nx.default_backend(EXLA.Backend)
+    vectorized_math = Nx.vectorize(base_math, :rows)
 
     on_exit(fn ->
       Nx.default_backend(backend)
     end)
+
+    %{base: base, vectorized: vectorized, base_math: base_math, vectorized_math: vectorized_math}
   end
 
   defn add_n(x, y), do: Nx.add(x, y)
@@ -39,18 +49,6 @@ defmodule Nx.VectorizeTest do
     EXLA.jit_apply(&equal_n/2, [x, y])
   end
 
-  describe "vectorize" do
-    test "adds new vectorization axes to the end of the list" do
-      v = Nx.vectorize(@vectorized, :cols)
-      assert v.vectorized_axes == [rows: 3, cols: 1]
-      assert v.shape == {3}
-    end
-
-    # TO-DO: re-write value inspect to support vectorization
-    @tag :skip
-    test "inspect works as expected"
-  end
-
   describe "unary math ops" do
     for {name, _} <- Nx.Shared.unary_math_funs(), name != :acosh do
       defn_name = :"#{name}_n"
@@ -60,32 +58,29 @@ defmodule Nx.VectorizeTest do
         EXLA.jit_apply(&unquote(defn_name)(&1), [x])
       end
 
-      test "Nx.#{name}/1 works on vectorized tensor" do
+      test "Nx.#{name}/1 works on vectorized tensor", %{base_math: base_math, vectorized_math: vectorized_math} do
         result =
-          @base_math
+          base_math
           |> Nx.unquote(name)()
           |> Nx.vectorize(:rows)
 
-        assert_equal(result, unquote(name)(@vectorized_math))
+        assert_equal(result, unquote(name)(vectorized_math))
       end
     end
 
-    test "Nx.acosh/1 works on vectorized tensor" do
-      expected =
-        @base_math
-        |> Nx.add(1)
-        |> Nx.acosh()
-        |> Nx.vectorize(:rows)
+    test "Nx.acosh/1 works on vectorized tensor", %{base_math: base_math, vectorized_math: vectorized_math} do
+      fun = EXLA.jit(&(&1 |> Nx.add(1) |> Nx.acosh()))
 
-      result = EXLA.jit_apply(&(&1 |> Nx.add(1) |> Nx.acosh()), [@vectorized_math])
+      expected = fun.(base_math) |> Nx.vectorize(:rows)
+      result = fun.(vectorized_math)
 
       assert_equal(expected, result)
     end
   end
 
   describe "binary operations" do
-    test "left addition by scalar" do
-      result = add(2, @vectorized)
+    test "left addition by scalar", %{vectorized: vectorized} do
+      result = add(2, vectorized)
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -97,8 +92,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "right addition by scalar" do
-      result = add(@vectorized, 2)
+    test "right addition by scalar", %{vectorized: vectorized} do
+      result = add(vectorized, 2)
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -110,8 +105,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "left addition by rank-1" do
-      result = add(Nx.tensor([2]), @vectorized)
+    test "left addition by rank-1", %{vectorized: vectorized} do
+      result = add(Nx.tensor([2]), vectorized)
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -123,8 +118,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "right addition by rank-1" do
-      result = add(@vectorized, Nx.tensor([2]))
+    test "right addition by rank-1", %{vectorized: vectorized} do
+      result = add(vectorized, Nx.tensor([2]))
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -136,8 +131,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "left addition by rank-2" do
-      result = add(Nx.tensor([[1], [2]]), @vectorized)
+    test "left addition by rank-2", %{vectorized: vectorized} do
+      result = add(Nx.tensor([[1], [2]]), vectorized)
       assert result.shape == {2, 3}
 
       assert_equal(
@@ -153,8 +148,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "right addition by rank-2" do
-      result = add(@vectorized, Nx.tensor([[1], [2]]))
+    test "right addition by rank-2", %{vectorized: vectorized} do
+      result = add(vectorized, Nx.tensor([[1], [2]]))
       assert result.shape == {2, 3}
 
       assert_equal(
@@ -170,30 +165,33 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "addition by vectorized with same axes" do
-      assert Nx.vectorize(Nx.add(@base, @base), :rows) == add(@vectorized, @vectorized)
+    test "addition by vectorized with same axes", %{base: base, vectorized: vectorized} do
+      assert_equal(
+        Nx.vectorize(Nx.add(base, base), :rows),
+        add(vectorized, vectorized)
+        )
     end
 
-    test "addition by vectorized with different axes" do
-      v2 = Nx.vectorize(@base, :cols)
+    test "addition by vectorized with different axes", %{vectorized: vectorized, base: base} do
+      v2 = Nx.vectorize(base, :cols)
 
       result =
         Nx.stack([
-          Nx.add(@base, Nx.tensor([[0, 1, 2]])),
-          Nx.add(@base, Nx.tensor([[3, 4, 5]])),
-          Nx.add(@base, Nx.tensor([[6, 7, 8]]))
+          Nx.add(base, Nx.tensor([[0, 1, 2]])),
+          Nx.add(base, Nx.tensor([[3, 4, 5]])),
+          Nx.add(base, Nx.tensor([[6, 7, 8]]))
         ])
         |> Nx.vectorize(:rows)
         |> Nx.vectorize(:cols)
 
-      assert_equal(result, add(@vectorized, v2))
+      assert_equal(result, add(vectorized, v2))
     end
 
-    test "addition by vectorized with common axes" do
+    test "addition by vectorized with common axes", %{vectorized: vectorized, base: base} do
       base_2 = Nx.iota({1, 2, 3, 1, 3})
       v2 = base_2 |> Nx.vectorize(:x) |> Nx.vectorize(:y) |> Nx.vectorize(:rows)
 
-      base = Nx.concatenate([@base, @base], axis: 1) |> Nx.reshape({1, 2, 3, 1, 3})
+      base = Nx.concatenate([base, base], axis: 1) |> Nx.reshape({1, 2, 3, 1, 3})
 
       result =
         Nx.add(base, base_2)
@@ -202,13 +200,13 @@ defmodule Nx.VectorizeTest do
         |> Nx.vectorize(:x)
         |> Nx.vectorize(:y)
 
-      assert_equal(result, add(@vectorized, v2))
+      assert_equal(result, add(vectorized, v2))
     end
   end
 
   describe "predicate operations" do
-    test "left Nx.equal by scalar" do
-      result = equal(2, @vectorized)
+    test "left Nx.equal by scalar", %{vectorized: vectorized} do
+      result = equal(2, vectorized)
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -220,8 +218,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "right Nx.equal by scalar" do
-      result = equal(@vectorized, 2)
+    test "right Nx.equal by scalar", %{vectorized: vectorized} do
+      result = equal(vectorized, 2)
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -233,8 +231,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "left Nx.equal by rank-1" do
-      result = equal(Nx.tensor([2]), @vectorized)
+    test "left Nx.equal by rank-1", %{vectorized: vectorized} do
+      result = equal(Nx.tensor([2]), vectorized)
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -246,8 +244,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "right Nx.equal by rank-1" do
-      result = equal(@vectorized, Nx.tensor([2]))
+    test "right Nx.equal by rank-1", %{vectorized: vectorized} do
+      result = equal(vectorized, Nx.tensor([2]))
       assert result.shape == {1, 3}
 
       assert_equal(
@@ -259,8 +257,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "left Nx.equal by rank-2" do
-      result = equal(Nx.tensor([[1], [2]]), @vectorized)
+    test "left Nx.equal by rank-2", %{vectorized: vectorized} do
+      result = equal(Nx.tensor([[1], [2]]), vectorized)
       assert result.shape == {2, 3}
 
       assert_equal(
@@ -279,8 +277,8 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "right Nx.equal by rank-2" do
-      result = equal(@vectorized, Nx.tensor([[1], [2]]))
+    test "right Nx.equal by rank-2", %{vectorized: vectorized} do
+      result = equal(vectorized, Nx.tensor([[1], [2]]))
       assert result.shape == {2, 3}
 
       assert_equal(
@@ -299,33 +297,33 @@ defmodule Nx.VectorizeTest do
       )
     end
 
-    test "Nx.equal by vectorized with same axes" do
+    test "Nx.equal by vectorized with same axes", %{vectorized: vectorized, base: base} do
       assert_equal(
-        Nx.vectorize(Nx.broadcast(Nx.tensor(1, type: :u8), @base), :rows),
-        equal(@vectorized, @vectorized)
+        Nx.vectorize(Nx.broadcast(Nx.tensor(1, type: :u8), base), :rows),
+        equal(vectorized, vectorized)
       )
     end
 
-    test "Nx.equal by vectorized with different axes" do
-      v2 = Nx.vectorize(@base, :cols)
+    test "Nx.equal by vectorized with different axes", %{vectorized: vectorized, base: base} do
+      v2 = Nx.vectorize(base, :cols)
 
       result =
         Nx.stack([
-          Nx.equal(@base, Nx.tensor([[0, 1, 2]])),
-          Nx.equal(@base, Nx.tensor([[3, 4, 5]])),
-          Nx.equal(@base, Nx.tensor([[6, 7, 8]]))
+          Nx.equal(base, Nx.tensor([[0, 1, 2]])),
+          Nx.equal(base, Nx.tensor([[3, 4, 5]])),
+          Nx.equal(base, Nx.tensor([[6, 7, 8]]))
         ])
         |> Nx.vectorize(:rows)
         |> Nx.vectorize(:cols)
 
-      assert_equal(result, equal(@vectorized, v2))
+      assert_equal(result, equal(vectorized, v2))
     end
 
-    test "Nx.equal by vectorized with common axes" do
+    test "Nx.equal by vectorized with common axes", %{vectorized: vectorized, base: base} do
       base_2 = Nx.iota({1, 2, 3, 1, 3})
       v2 = base_2 |> Nx.vectorize(:x) |> Nx.vectorize(:y) |> Nx.vectorize(:rows)
 
-      base = Nx.concatenate([@base, @base], axis: 1) |> Nx.reshape({1, 2, 3, 1, 3})
+      base = Nx.concatenate([base, base], axis: 1) |> Nx.reshape({1, 2, 3, 1, 3})
 
       result =
         Nx.equal(base, base_2)
@@ -334,7 +332,7 @@ defmodule Nx.VectorizeTest do
         |> Nx.vectorize(:x)
         |> Nx.vectorize(:y)
 
-      assert_equal(result, equal(@vectorized, v2))
+      assert_equal(result, equal(vectorized, v2))
     end
   end
 end
