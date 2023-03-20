2d61403dee829822434b589a0ea2faef4c50c2f6
José Valim <jose.valim@dashbit.co>
2023-03-20 10:07:39 +0100

    Support hidden nodes in Nx.Serving

--- nx/lib/nx/application.ex
+++ nx/lib/nx/application.ex
@@ -4,9 +4,43 @@ defmodule Nx.Application do
 
   def start(_type, _args) do
     children = [
-      %{id: Nx.Serving.PG, start: {:pg, :start_link, [Nx.Serving.PG]}}
+      %{id: Nx.Serving.PG, start: {:pg, :start_link, [Nx.Serving.PG]}},
+      {Nx.HiddenServing, Nx.Serving.PG}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_all)
+  end
+end
+
+defmodule Nx.HiddenServing do
+  # Module to connect hidden nodes with serving
+  @moduledoc false
+  use GenServer
+
+  @doc false
+  def start_link(scope) do
+    GenServer.start_link(__MODULE__, scope)
+  end
+
+  @impl true
+  def init(scope) do
+    pid = Process.whereis(scope)
+    :ok = :net_kernel.monitor_nodes(true, node_type: :hidden)
+
+    for node <- Node.list(:hidden) do
+      send({scope, node}, {:discover, pid})
+    end
+
+    {:ok, scope}
+  end
+
+  @impl true
+  def handle_info({:nodeup, node, _}, scope) when node != node() do
+    send({scope, node}, {:discover, Process.whereis(scope)})
+    {:noreply, scope}
+  end
+
+  def handle_info(_msg, scope) do
+    {:noreply, scope}
   end
 end
--- nx/test/test_helper.exs
+++ nx/test/test_helper.exs
@@ -5,9 +5,13 @@ cond do
     :ok
 
   Code.ensure_loaded?(:peer) and match?({:ok, _}, Node.start(:"primary@127.0.0.1", :longnames)) ->
-    {:ok, _pid, node} = :peer.start(%{name: :"secondary@127.0.0.1"})
-    true = :erpc.call(node, :code, :set_path, [:code.get_path()])
-    {:ok, _} = :erpc.call(node, :application, :ensure_all_started, [:nx])
+    {:ok, _pid, node2} = :peer.start(%{name: :"secondary@127.0.0.1"})
+    {:ok, _pid, node3} = :peer.start(%{name: :"tertiary@127.0.0.1", args: ~w(-hidden)c})
+
+    for node <- [node2, node3] do
+      true = :erpc.call(node, :code, :set_path, [:code.get_path()])
+      {:ok, _} = :erpc.call(node, :application, :ensure_all_started, [:nx])
+    end
 
   true ->
     ExUnit.configure(exclude: [:distributed])
