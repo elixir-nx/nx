b8d2b210f12b47c14ba09ae58fafa1d756c219b9
Mateusz <msluszniak@student.agh.edu.pl>
2023-05-03 14:36:26 +0200

    fix condition

--- nx/lib/nx.ex
+++ nx/lib/nx.ex
@@ -15475,7 +15475,7 @@ defmodule Nx do
   """
   @doc type: :element
   def log_p(tensor, base) do
-    if base < 0 or base == 1 do
+    if base <= 0 or base == 1 do
       raise ArgumentError,
             "expected base to be greater than 0 and different then 1, got: #{inspect(base)}"
     end
