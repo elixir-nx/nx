b18cb65782b90300f29fc6293895f7ed63f7e310
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-05-15 07:49:03 -0300

    fix: flaky distributed tests

--- nx/test/test_helper.exs
+++ nx/test/test_helper.exs
@@ -4,7 +4,8 @@ cond do
   :distributed in Keyword.get(ExUnit.configuration(), :exclude, []) ->
     :ok
 
-  Code.ensure_loaded?(:peer) and match?({:ok, _}, Node.start(:"primary@127.0.0.1", :longnames)) ->
+  Code.ensure_loaded?(:peer) and {"", 0} == System.cmd("epmd", ["-daemon"]) and
+      match?({:ok, _}, Node.start(:"primary@127.0.0.1", :longnames)) ->
     {:ok, _pid, node2} = :peer.start(%{name: :"secondary@127.0.0.1"})
     {:ok, _pid, node3} = :peer.start(%{name: :"tertiary@127.0.0.1", args: ~w(-hidden)c})
 
