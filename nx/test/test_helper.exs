0e96a3ff9da581d1df0b1bb4853e43574ca2157a
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-05-15 22:56:36 -0300

    fix: flaky distributed tests (#1218)

--- nx/test/test_helper.exs
+++ nx/test/test_helper.exs
@@ -1,10 +1,21 @@
 ExUnit.start(assert_receive_timeout: 1000)
 
+try_starting_epmd? = fn ->
+  case :os.type() do
+    {:unix, _} ->
+      {"", 0} == System.cmd("epmd", ["-daemon"])
+
+    _ ->
+      true
+  end
+end
+
 cond do
   :distributed in Keyword.get(ExUnit.configuration(), :exclude, []) ->
     :ok
 
-  Code.ensure_loaded?(:peer) and match?({:ok, _}, Node.start(:"primary@127.0.0.1", :longnames)) ->
+  Code.ensure_loaded?(:peer) and try_starting_epmd?.() and
+      match?({:ok, _}, Node.start(:"primary@127.0.0.1", :longnames)) ->
     {:ok, _pid, node2} = :peer.start(%{name: :"secondary@127.0.0.1"})
     {:ok, _pid, node3} = :peer.start(%{name: :"tertiary@127.0.0.1", args: ~w(-hidden)c})
 
