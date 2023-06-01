7b52a79fb1a0e610408026fd51c04c6293a841dd
Tiago Davi <tiago.asp.net@gmail.com>
2023-06-01 16:16:02 -0300

    add split function

--- nx/lib/nx.ex
+++ nx/lib/nx.ex
@@ -3265,6 +3265,61 @@ defmodule Nx do
     end)
   end
 
+  @doc ~S"""
+  Split tensor or list into train and test subsets.
+
+  ## Examples
+
+    Split into 80% for training and 20% for testing.
+
+    iex> {_train, _test} = Nx.split(Nx.tensor([[3, 6, 5], [26, 75, 3], [23, 4, 1]]), split: 0.8)
+    {#Nx.Tensor<
+      s64[2][3]
+      [
+        [3, 6, 5],
+        [26, 75, 3]
+      ]
+      >, #Nx.Tensor<
+        s64[1][3]
+        [
+          [23, 4, 1]
+        ]
+      >
+    }
+
+    iex> {_train, _test} = Nx.split([[3, 6, 5], [26, 75, 3], [23, 4, 1]], split: 0.8)
+    {[[3, 6, 5], [26, 75, 3]], [[23, 4, 1]]}
+  """
+  @doc type: :shape
+  def split(tensor, opts \\ [])
+
+  def split(tensor, opts) when is_tensor(tensor) do
+    {train, test} =
+      tensor
+      |> to_list()
+      |> split_op(opts)
+
+    {Nx.tensor(train), Nx.tensor(test)}
+  end
+
+  def split(data, opts) when is_list(data) do
+    split_op(data, opts)
+  end
+
+  defp split_op([_ | _] = features, opts) do
+    split = Keyword.fetch!(opts, :split)
+
+    if is_float(split) and split > 0.0 and split <= 1.0 do
+      num_samples = Enum.count(features)
+
+      split_size = Kernel.floor(split * num_samples)
+
+      Enum.split(features, split_size)
+    else
+      raise ":split must be a float number between 0.0 and 1.0"
+    end
+  end
+
   @doc """
   Broadcasts `tensor` to the given `broadcast_shape`.
 
--- nx/test/nx_test.exs
+++ nx/test/nx_test.exs
@@ -3022,4 +3022,63 @@ defmodule NxTest do
       assert_all_close(zeros, Nx.imag(x_ifft), atol: 1.0e-8)
     end
   end
+
+  describe "split/2" do
+    test "Split list into 50% for training and 50% for testing" do
+      tensor = Nx.iota({10, 2}, names: [:x, :y])
+
+      {train, test} =
+        tensor
+        |> Nx.to_list()
+        |> Nx.split(split: 0.5)
+
+      assert [
+               [0, 1],
+               [2, 3],
+               [4, 5],
+               [6, 7],
+               [8, 9]
+             ] == train
+
+      assert [
+               [10, 11],
+               [12, 13],
+               [14, 15],
+               [16, 17],
+               [18, 19]
+             ] == test
+    end
+
+    test "Split into 70% for training and 30% for testing" do
+      tensor = Nx.iota({100, 6}, names: [:x, :y])
+      {train, test} = Nx.split(tensor, split: 0.7)
+
+      assert length(Nx.to_list(train)) == 70
+      assert length(Nx.to_list(test)) == 30
+    end
+
+    test "Split into 75% for training and 25% for testing" do
+      tensor = Nx.iota({100, 10}, names: [:x, :y])
+      {train, test} = Nx.split(tensor, split: 0.75)
+
+      assert length(Nx.to_list(train)) == 75
+      assert length(Nx.to_list(test)) == 25
+    end
+
+    test "Split into 61% for training and 39% for testing" do
+      tensor = Nx.iota({100, 10}, names: [:x, :y])
+      {train, test} = Nx.split(tensor, split: 0.61)
+
+      assert length(Nx.to_list(train)) == 61
+      assert length(Nx.to_list(test)) == 39
+    end
+
+    test "Split into 60% for training and 40% for testing with unbalanced data" do
+      tensor = Nx.iota({73, 4}, names: [:x, :y])
+      {train, test} = Nx.split(tensor, split: 0.61)
+
+      assert length(Nx.to_list(train)) == 44
+      assert length(Nx.to_list(test)) == 29
+    end
+  end
 end
