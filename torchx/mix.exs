63d713711641ab0a0f2a23f31d7bbee30df68db6
José Valim <jose.valim@dashbit.co>
2023-07-19 20:51:04 +0200

    Update dll_loader_helper

--- torchx/mix.exs
+++ torchx/mix.exs
@@ -70,7 +70,7 @@ defmodule Torchx.MixProject do
     [
       # {:nx, "~> 0.5.1"},
       {:nx, path: "../nx"},
-      {:dll_loader_helper, "~> 0.1.0"},
+      {:dll_loader_helper, "~> 0.1 or ~> 1.0"},
       {:elixir_make, "~> 0.6"},
       {:ex_doc, "~> 0.29.0", only: :docs}
     ]
