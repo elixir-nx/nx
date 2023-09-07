6ebcc813b3178e2fd8a8a52c01a5071647b54368
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-09-07 11:40:29 -0300

    a few more test cases

--- candlex/test/candlex_test.exs
+++ candlex/test/candlex_test.exs
@@ -195,36 +195,18 @@ defmodule CandlexTest do
       |> assert_equal(t([[1, 1, 1], [0, 0, 0]]))
     end
 
-    # test "left_shift" do
-    #   Nx.left_shift(1, 0)
-    #   |> assert_equal(t(1))
-
-    #   t([1, 2, 3])
-    #   |> Nx.left_shift(2)
-    #   |> assert_equal(t([4, 8, 12]))
-
-    #   t([1, 1, -1, -1])
-    #   |> Nx.left_shift(t([1, 2, 3, 4]))
-    #   |> assert_equal(t([2, 4, -8, -16]))
-    # end
-
-    # test "right_shift" do
-    #   Nx.right_shift(1, 0)
-    #   |> assert_equal(t(1))
-
-    #   t([2, 4, 8])
-    #   |> Nx.right_shift(2)
-    #   |> assert_equal(t([0, 1, 2]))
-
-    #   t([16, 32, -64, -128])
-    #   |> Nx.right_shift(t([1, 2, 3, 4]))
-    #   |> assert_equal(t([8, 8, -8, -8]))
-    # end
-
     test "bitcast" do
       t([0, 0, 0], type: :s64)
       |> Nx.bitcast(:f64)
       |> assert_equal(t([0.0, 0.0, 0.0]))
+
+      t([0, 0, 0], type: :u32)
+      |> Nx.bitcast(:f32)
+      |> assert_equal(t([0.0, 0.0, 0.0]))
+
+      t([0, 0, 0], type: :u32)
+      |> Nx.bitcast(:u32)
+      |> assert_equal(t([0, 0, 0]))
     end
 
     test "eye" do
@@ -638,6 +620,22 @@ defmodule CandlexTest do
       t([1, 1, -1, -1])
       |> Nx.left_shift(t([1, 2, 3, 4]))
       |> assert_equal(t([2, 4, -8, -16]))
+
+      t([1, 2, 3], type: :u32)
+      |> Nx.left_shift(2)
+      |> assert_equal(t([4, 8, 12]))
+
+      t([1, 2, 3], type: :u32)
+      |> Nx.left_shift(t(2, type: :u8))
+      |> assert_equal(t([4, 8, 12]))
+
+      t([1, 1, 0, 0], type: :u32)
+      |> Nx.left_shift(t([1, 2, 3, 4]))
+      |> assert_equal(t([2, 4, 0, 0]))
+
+      t([1, 1, 0, 0], type: :u32)
+      |> Nx.left_shift(t([1, 2, 3, 4], type: :u8))
+      |> assert_equal(t([2, 4, 0, 0]))
     end
 
     test "right_shift" do
@@ -651,6 +649,14 @@ defmodule CandlexTest do
       t([16, 32, -64, -128])
       |> Nx.right_shift(t([1, 2, 3, 4]))
       |> assert_equal(t([8, 8, -8, -8]))
+
+      t([2, 4, 8], type: :u32)
+      |> Nx.right_shift(2)
+      |> assert_equal(t([0, 1, 2]))
+
+      t([16, 32, -64, -128], type: :u32)
+      |> Nx.right_shift(t([1, 2, 3, 4]))
+      |> assert_equal(t([8, 8, 536870904, 268435448]))
     end
 
     test "is_infinity" do
