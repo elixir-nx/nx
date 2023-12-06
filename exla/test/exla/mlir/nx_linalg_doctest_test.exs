81df9fdcbe17434bfa1f5c4d02e7e7cf577c5f2d
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-12-05 23:29:41 -0300

    chore: remove stray commnets

--- exla/test/exla/mlir/nx_linalg_doctest_test.exs
+++ exla/test/exla/mlir/nx_linalg_doctest_test.exs
@@ -19,41 +19,6 @@ defmodule EXLA.MLIR.NxLinAlgDoctestTest do
     matrix_power: 2
   ]
   @rounding_error_doctests [triangular_solve: 3]
-  # @argument_count_error_doctests [
-  #   stack: 2,
-  #   reflect: 2,
-  #   concatenate: 2,
-  #   to_batched: 3,
-  #   cumulative_sum: 2,
-  #   cumulative_product: 2,
-  #   cumulative_min: 2,
-  #   cumulative_max: 2,
-  #   indexed_add: 4,
-  #   indexed_put: 4,
-  #   mode: 2,
-  #   slice: 4
-  # ]
-  # @sign_error_doctests [logical_not: 1, ceil: 1, conjugate: 1]
-  # @incorrect_results_error_doctests [
-  #   equal: 2,
-  #   greater: 2,
-  #   less_equal: 2,
-  #   greater_equal: 2,
-  #   less: 2,
-  #   is_nan: 1,
-  #   tril: 2,
-  #   tri: 3,
-  #   sort: 2,
-  #   is_infinity: 1,
-  #   argsort: 2
-  # ]
-  # @excluded_doctests @argument_count_error_doctests ++
-  #                      @function_clause_error_doctests ++
-  #                      @rounding_error_doctests ++
-  #                      @invalid_type_error_doctests ++
-  #                      @sign_error_doctests ++
-  #                      @incorrect_results_error_doctests ++
-  #                      [:moduledoc]
 
   @excluded_doctests @function_clause_error_doctests ++
                        @rounding_error_doctests ++
