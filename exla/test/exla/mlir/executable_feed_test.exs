a9221b5f02a8a273036757f44a7afb64bf5961e4
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-12-05 23:32:31 -0300

    chore: add MLIR Nx doctests (#1394)

---
+++ exla/test/exla/mlir/executable_feed_test.exs
@@ -0,0 +1,53 @@
+defmodule EXLA.MLIR.ExecutableFeedTest do
+  # infeed/outfeed are global resources, so they either
+  # need to be locked or we cannot run them concurrently.
+  use ExUnit.Case, async: false
+
+  alias EXLA.BinaryBuffer
+  alias EXLA.DeviceBuffer
+  alias EXLA.Client
+  alias EXLA.Shape
+  alias EXLA.MLIR.Value
+  import EXLAHelpers
+
+  describe "infeed/outfeed" do
+    test "successfully sends to/from device asynchronously" do
+      t = BinaryBuffer.from_binary(<<1::32-native>>, Shape.make_shape({:s, 32}, {}))
+
+      task =
+        Task.async(fn ->
+          :timer.tc(fn ->
+            run_one([], [compiler_mode: :mlir], Nx.template({}, {:s, 32}), fn b ->
+              token = Value.create_token(b)
+              val_and_token = Value.infeed(token, t.shape)
+              val = Value.get_tuple_element(val_and_token, 0)
+              new_token = Value.get_tuple_element(val_and_token, 1)
+              outfeed_val = Value.add(val, val)
+
+              _outfeed_token = Value.outfeed(new_token, [outfeed_val])
+              Value.tuple([Value.add(outfeed_val, val)])
+            end)
+          end)
+        end)
+
+      refute_received _
+      # sleep here to ensure that we have a known measureable delay in the task's execution
+      # that is correlated to how much time we wait for the infeed to have something in the queue
+      Process.sleep(1_000)
+      assert :ok = Client.to_infeed(client(), 0, [{t.data, t.shape}])
+      assert from_outfeed(client(), 0, Shape.make_shape({:s, 32}, {})) == <<2::32-native>>
+      assert {time, [a = %DeviceBuffer{}]} = Task.await(task)
+      assert time >= 1_000
+      assert DeviceBuffer.read(a) == <<3::32-native>>
+    end
+  end
+
+  defp from_outfeed(client, device_id, shape) do
+    ref = make_ref()
+    Client.from_outfeed(client, device_id, [shape], self(), ref)
+
+    receive do
+      {^ref, msg} -> msg
+    end
+  end
+end
---
+++ exla/test/exla/mlir/nx_linalg_doctest_test.exs
@@ -0,0 +1,28 @@
+defmodule EXLA.MLIR.NxLinAlgDoctestTest do
+  use EXLA.Case, async: true
+
+  setup do
+    Nx.default_backend(EXLA.Backend)
+    Nx.Defn.default_options(compiler: EXLA, compiler_mode: :mlir)
+  end
+
+  @invalid_type_error_doctests [svd: 2, pinv: 2, matrix_rank: 2]
+  @function_clause_error_doctests [
+    norm: 2,
+    eigh: 2,
+    cholesky: 1,
+    lu: 2,
+    solve: 2,
+    determinant: 1,
+    qr: 2,
+    invert: 1,
+    matrix_power: 2
+  ]
+  @rounding_error_doctests [triangular_solve: 3]
+
+  @excluded_doctests @function_clause_error_doctests ++
+                       @rounding_error_doctests ++
+                       @invalid_type_error_doctests ++
+                       [:moduledoc]
+  doctest Nx.LinAlg, except: @excluded_doctests
+end
