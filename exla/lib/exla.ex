06132373c529cbe2ae4cb51636312cf68072e1af
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-02-20 18:07:00 -0300

    chore: remove stray comment

--- exla/lib/exla.ex
+++ exla/lib/exla.ex
@@ -71,11 +71,11 @@ defmodule EXLA do
       * `:highest` - Slowest but most accurate. Performs computations in float32
         or float64 as applicable
 
-     * `:compiler_mode` - the mode to use for the compiler. It must be one of:
+    * `:compiler_mode` - the mode to use for the compiler. It must be one of:
 
-       * `:mlir` - The default mode. Uses MLIR to compile the computation
+      * `:mlir` - The default mode. Uses MLIR to compile the computation
 
-       * `:xla` - Legacy implementation. Uses XLA to compile the computation
+      * `:xla` - Legacy implementation. Uses XLA to compile the computation
 
     The `:compiler_mode` can also be set globally through a specific fallback config:
 
