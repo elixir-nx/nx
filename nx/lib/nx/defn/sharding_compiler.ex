65853536398a5ad47bc6186ad4590088164cc180
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-09-25 20:49:27 -0300

    proof of concept solution

--- nx/lib/nx/defn/sharding_compiler.ex
+++ nx/lib/nx/defn/sharding_compiler.ex
@@ -1,12 +1,50 @@
 defmodule Nx.Defn.ShardingCompiler do
   @behaviour Nx.Defn.Compiler
 
+  def build_comprehension(slices) do
+    args = Macro.generate_arguments(length(slices), nil)
 
-  def __jit__(key, vars, fun, args_list, opts) do
-    __compile__(key, vars, fun, opts).(args_list)
+    generators =
+      Enum.zip_with(args, slices, fn arg, slice ->
+        quote do
+          unquote(arg) <- unquote(Macro.escape(slice))
+        end
+      end)
+
+    {fun, []} =
+      Code.eval_quoted(
+        quote do
+          fn arg ->
+            for unquote_splicing(generators) do
+              arg[unquote(args)]
+            end
+          end
+        end
+      )
+
+    fun
   end
 
-  def __compile__(_key, vars, fun, opts) do
+  def compile(fun, args, shard_args) do
+    %{data: %{sharding_config: output_config}} = apply(fun, shard_args)
 
+    slices =
+      Enum.map(output_config, fn
+        nil -> [..]
+        %{slices: slices} -> slices
+      end)
+
+    dbg(slices)
+
+    slicer = build_comprehension(slices)
+
+    shards = Enum.map(args, slicer)
+
+    funs =
+      Enum.zip_with(shards, fn args ->
+        Nx.Defn.compile(fun, args, compiler: Nx.Defn.Evaluator)
+      end)
+
+    {shards, funs}
   end
 end
