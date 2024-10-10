4bb2d97be6240be0c61b86178aad33d4e42f1bcd
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-10-10 00:54:31 -0300

    feat: support implicit broadcasting

--- nx/lib/nx/defn/sharding_compiler.ex
+++ nx/lib/nx/defn/sharding_compiler.ex
@@ -5,7 +5,7 @@ defmodule Nx.Defn.ShardingCompiler do
 
   @behaviour Nx.Defn.Compiler
 
-  defstruct [:id, :tensor_sharding, :input_tensor_shardings]
+  defstruct [:id, :tensor_sharding, :input_tensor_shardings, :parameter_ids_to_index]
 
   defmodule Shard do
     import Inspect.Algebra
@@ -248,20 +248,117 @@ defmodule Nx.Defn.ShardingCompiler do
         type: type,
         data: %__MODULE__{
           input_tensor_shardings: input_tensor_shardings,
-          tensor_sharding: output_config
+          tensor_sharding: output_config,
+          parameter_ids_to_index: parameter_ids_to_index
         }
       }
     ] =
       __compile__(key, vars, fun, sharding_config: opts[:sharding_config]).([args])
 
-    dbg(output_config)
+    data_sections =
+      output_config.shards |> Enum.sort_by(fn {axis, _} -> axis end) |> cartesian_product()
 
-    data_sections = cartesian_product(Enum.to_list(output_config.shards))
+    # Find the parents for each data section
+    # Group by inputs
+    # For each input, sort the shards by axis
+    # For each axis, find the minimum start and the maximum end (we need to test for slicing inside the code as well)
+    # it might be the case where an axis is not present in the mapping. This means we need the full axis.
 
-    dbg(data_sections)
+    result =
+      for section <- data_sections do
+        dbg(section)
 
-    raise "asdf"
-    # [{output_holder, result}]
+        shards_by_input_id =
+          section
+          |> Enum.flat_map(fn {_axis, shard} ->
+            get_root_parents(shard)
+          end)
+          |> Enum.group_by(fn shard -> shard.input_id end)
+
+        inputs_by_index =
+          parameter_ids_to_index
+          |> Enum.sort_by(fn {_id, idx} -> idx end)
+          |> Enum.map(fn {id, idx} -> {id, Enum.fetch!(args, idx)} end)
+
+        sliced_inputs =
+          for {input_id, input_fn} <- inputs_by_index do
+            input = input_fn.()
+            shards = shards_by_input_id[input_id]
+            shards_by_axis = Enum.group_by(shards, & &1.axis)
+
+            {_, _, starts_reverse, lengths_reverse} =
+              Enum.reduce(Tuple.to_list(input.shape), {shards_by_axis, 0, [], []}, fn axis_size,
+                                                                                      {shards_by_axis,
+                                                                                       axis,
+                                                                                       starts,
+                                                                                       lengths} ->
+                {shards, shards_by_axis} = Map.pop(shards_by_axis, axis)
+
+                {starts, lengths} =
+                  if shards do
+                    min_start = Enum.min(Enum.map(shards, & &1.start))
+                    max_end = Enum.max(Enum.map(shards, &(&1.start + &1.length - 1)))
+
+                    starts = [min_start | starts]
+                    lengths = [max_end - min_start + 1 | lengths]
+                    {starts, lengths}
+                  else
+                    starts = [0 | starts]
+                    lengths = [axis_size | lengths]
+                    {starts, lengths}
+                  end
+
+                {shards_by_axis, axis + 1, starts, lengths}
+              end)
+
+            starts = Enum.reverse(starts_reverse)
+            lengths = Enum.reverse(lengths_reverse)
+
+            Nx.slice(input, starts, lengths)
+          end
+
+        {out_starts, []} =
+          Enum.map_reduce(0..(tuple_size(shape) - 1)//1, section, fn
+            axis, [{axis, shard} | shards] ->
+              {shard.start, shards}
+
+            _axis, shards ->
+              {0, shards}
+          end)
+
+        caster_fn = fn result, acc ->
+          Nx.put_slice(acc, out_starts, result)
+        end
+
+        sharding_compiler = opts[:sharding_compiler]
+        sharding_compiler_options = opts[:sharding_compiler_options]
+
+        vars =
+          Enum.with_index(sliced_inputs, fn arg, idx ->
+            arg
+            # |> Expr.tensor()
+            |> Expr.parameter(:root, idx)
+          end)
+
+        compiled_fun =
+          sharding_compiler.__compile__({key, section}, vars, fun, sharding_compiler_options)
+
+        shard_fn = fn [args] ->
+          [res] =
+            compiled_fun.([
+              Enum.map(Tuple.to_list(args), fn arg ->
+                fn -> arg end
+              end)
+            ])
+
+          res
+        end
+
+        {[List.to_tuple(sliced_inputs)], shard_fn, caster_fn}
+      end
+
+    output_holder = Nx.iota(shape, type: type)
+    [{output_holder, result}]
   end
 
   defp cartesian_product([{axis, first} | rest]) do
@@ -300,7 +397,10 @@ defmodule Nx.Defn.ShardingCompiler do
           %{}
         )
 
-      [put_in(container.data.input_tensor_shardings, state_shard_configs)]
+      container = put_in(container.data.input_tensor_shardings, state_shard_configs)
+      container = put_in(container.data.parameter_ids_to_index, state.parameter_ids_to_index)
+
+      [container]
     end
   end
 
@@ -339,7 +439,7 @@ defmodule Nx.Defn.ShardingCompiler do
     config = Map.fetch!(state.tensor_shardings, i)
     res = tensor |> Nx.devectorize() |> shard(config, input_id: id)
 
-    state = put_in(state.parameter_ids_to_index[res.data.id], i)
+    state = put_in(state.parameter_ids_to_index[id], i)
     {res, {Map.put(cache, id, res), state}}
   end
 
