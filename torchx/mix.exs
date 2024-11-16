be4ad6688bba8d35632be3dd04aef01c7f4c9b2b
José Valim <jose.valim@dashbit.co>
2024-11-16 12:23:03 +0100

    Release v0.9.2

--- torchx/mix.exs
+++ torchx/mix.exs
@@ -2,7 +2,7 @@ defmodule Torchx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/elixir-nx/nx"
-  @version "0.9.1"
+  @version "0.9.2"
 
   @libtorch_compilers [:torchx, :cmake]
 
