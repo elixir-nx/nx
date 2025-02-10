22aab8f08475805f77206288591d3ea5b687f59e
TomasPegado <lenzitomas00@gmail.com>
2025-02-10 10:35:53 -0300

    docs: add installation guide

--- nx/mix.exs
+++ nx/mix.exs
@@ -59,6 +59,7 @@ defmodule Nx.MixProject do
         "CHANGELOG.md",
         "guides/intro-to-nx.livemd",
         "guides/getting_started/introduction.md",
+        "guides/getting_started/installation.md",
         "guides/getting_started/quickstart.livemd",
         "guides/advanced/vectorization.livemd",
         "guides/advanced/aggregation.livemd",
@@ -114,7 +115,7 @@ defmodule Nx.MixProject do
         ]
       ],
       groups_for_extras: [
-        Getting_Started: ~r"^guides/getting_started/",
+        "Getting Started": ~r"^guides/getting_started/",
         Exercises: ~r"^guides/exercises/",
         Advanced: ~r"^guides/advanced/"
       ]
