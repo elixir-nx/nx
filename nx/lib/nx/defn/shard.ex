390997d0af407ccda69034a0427174db5feb13a6
Chapaman <14204271+Chapaman@users.noreply.github.com>
2025-12-15 09:17:11 -0300

    fixed some arity issues

--- nx/lib/nx/defn/shard.ex
+++ nx/lib/nx/defn/shard.ex
@@ -1,7 +1,11 @@
 defmodule Nx.Defn.Shard do
 
+  # Exemplo original
+  # @callback __shard_jit__(fun, mesh, opts)
+
   @callback __shard_jit__(
     key :: term,
+    mesh :: Nx.Defn.Shard.Mesh.t(),
     vars,
     fun :: (vars -> Nx.Container.t()),
     args_list :: [[(-> Nx.Tensor.t())]],
@@ -9,24 +13,43 @@ defmodule Nx.Defn.Shard do
   ) :: [Nx.Container.t()]
   when vars: [Nx.Container.t()]
 
-  def __shard_jit__(fun, params, args_list, opts) do
-    {compiler, runtime_fun, opts} = prepare_options(fun, opts)
-    compiler.__shard_jit__(fun, params, runtime_fun, args_list, opts)
+  def __shard_jit__(fun, mesh, params, args_list, opts) do
+    {module, runtime_fun, opts} = prepare_options(fun, mesh, opts)
+    module.__shard_jit__(fun, mesh, params, runtime_fun, args_list, opts)
   rescue
     e in [UndefinedFunctionError] ->
-      raise_missing_callback(e, :__shard_jit__, 5, __STACKTRACE__)
+      raise_missing_callback(e, :__shard_jit__, 6, __STACKTRACE__)
   end
 
-  def shard_jit(fun, opts \\ []) when is_function(fun) and is_list(opts) do
-    wrap(fun, &shard_jit_apply(fun, &1, opts))
+  def shard_jit(fun, mesh, opts \\ []) when is_function(fun) and is_list(opts) do
+    wrap(fun, &shard_jit_apply(fun, mesh, &1, opts))
+  end
+
+  def shard_jit_apply(fun, mesh, args, opts \\ [])
+    when is_function(fun) and is_list(args) and is_list(opts) do
+  {on_conflict, opts} = Keyword.pop(opts, :on_conflict, :raise)
+
+    cond do
+      Nx.Defn.Shard.current() == nil ->
+        do_shard_jit_apply(fun, mesh, args, opts)
+
+      on_conflict == :raise ->
+        raise "cannot invoke Shard JITed function when there is a Shard JIT compilation happening"
+
+      on_conflict == :force ->
+        do_shard_jit_apply(fun, mesh, args, opts)
+
+      on_conflict == :reuse ->
+        apply(fun, args)
+    end
   end
 
 
   defp raise_missing_callback(exception, name, arity, stacktrace) do
     case exception do
-      %UndefinedFunctionError{module: compiler, function: ^name, arity: ^arity} ->
+      %UndefinedFunctionError{module: module, function: ^name, arity: ^arity} ->
         raise ArgumentError,
-              "the expected compiler callback #{name}/#{arity} is missing. Please check that the module #{inspect(compiler)} is an Nx.Defn.Compiler."
+              "the expected shard callback #{name}/#{arity} is missing. Please check that the module #{inspect(module)} is an Nx.Defn.Shard."
 
       _ ->
         # This is not an error that should've been caught by this function, so we pass the exception along
@@ -34,26 +57,44 @@ defmodule Nx.Defn.Shard do
     end
   end
 
-  defp prepare_options(fun, opts) do
-    {compiler, opts} = Keyword.pop(opts, :compiler, Nx.Defn.Evaluator)
-    {compiler, &runtime_fun(&1, fun, compiler), opts}
+  defp prepare_options(fun, mesh, opts) do
+    {module, opts} = Keyword.pop(opts, :module, Nx.Defn.Evaluator)
+    {module, &runtime_fun(&1, fun, mesh, module), opts}
   end
 
+  defp runtime_fun(args, fun, mesh, module) do
+    previous_backend = Process.put(Nx.Shared.backend_pdict_key(), {Nx.Defn.Expr, []})
+    previous = Process.put(Nx.Defn.Shard, module)
 
+    try do
+      fun
+      |> apply(args)
+      |> Nx.Defn.Composite.traverse(&Nx.Defn.Expr.tensor/1)
+    after
+      if previous_backend do
+        Process.put(Nx.Shared.backend_pdict_key(), previous_backend)
+      else
+        Process.delete(Nx.Shared.backend_pdict_key())
+      end
 
-
-
-
-  def jit(fun, mesh, opts \\ []) do
-    ...
+      if previous do
+        Process.put(Nx.Defn.Shard, module)
+      else
+        Process.delete(Nx.Defn.Shard)
+      end
+    end
   end
 
-  def jit_apply(fun, mesh, args, opts \\ []) do
-    jit(fun, mesh, opts).(args)
-  end
+  defp do_shard_jit_apply(fun, mesh, args, opts) do
+  opts = prepare_options(opts)
+  {fun, params, _templates, flatten} = Nx.Defn.Compiler.to_lazy_params(fun, args)
+  [res] = Nx.Defn.Shard.__shard_jit__(fun, mesh, params, [flatten], opts)
+  res
 end
 
 defmodule Nx.Defn.Shard.Mesh do
   defstruct [:name, :shape]
   @type t :: %__MODULE__{name: String.t(), shape: tuple()}
+end
+
 end
